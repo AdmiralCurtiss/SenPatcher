0faa49dbee2ea32c8b408ccc8f807a5d01a6f763
Admiral H. Curtiss <pikachu025@gmail.com>
2020-07-08 23:15:41 +0200

    Add file decompression support to SenLib.

---
+++ SenLib/DecompressHelper.cs
@@ -0,0 +1,146 @@
+﻿using HyoutaUtils;
+using HyoutaUtils.Streams;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SenLib {
+	public static class DecompressHelper {
+		public static Stream DecompressFromBuffer(byte[] buffer) {
+			return DecompressFromStream(new DuplicatableByteArrayStream(buffer));
+		}
+
+		public static Stream DecompressFromStream(Stream instream) {
+			instream.Position = 0;
+			int filter = instream.ReadUInt8();
+
+			if (filter == 0) {
+				uint crc = instream.ReadUInt32(EndianUtils.Endianness.LittleEndian);
+				byte[] properties = instream.ReadBytes(5);
+				long length = instream.ReadInt64(EndianUtils.Endianness.LittleEndian);
+				Stream outstream = new MemoryStream((int)length);
+
+				SevenZip.Compression.LZMA.Decoder decoder = new SevenZip.Compression.LZMA.Decoder();
+				decoder.SetDecoderProperties(properties);
+				long compressedSize = instream.Length - instream.Position;
+				decoder.Code(instream, outstream, compressedSize, length, null);
+
+				if (!SevenZip.CRC.VerifyDigest(crc, outstream.CopyToByteArray(), 0, (uint)outstream.Length)) {
+					throw new Exception("crc mismatch");
+				}
+
+				outstream.Position = 0;
+				return outstream;
+			}
+
+			if (filter == 1) {
+				uint crc = instream.ReadUInt32(EndianUtils.Endianness.LittleEndian);
+				byte[] properties = instream.ReadBytes(5);
+				long length = instream.ReadInt64(EndianUtils.Endianness.LittleEndian);
+				Stream outstream = new MemoryStream((int)length);
+
+				SevenZip.Compression.LZMA.Decoder decoder = new SevenZip.Compression.LZMA.Decoder();
+				decoder.SetDecoderProperties(properties);
+				long compressedSize = instream.Length - instream.Position;
+				decoder.Code(instream, outstream, compressedSize, length, null);
+
+				MemoryStream defilteredstream = new MemoryStream((int)outstream.Length);
+				outstream.Position = 0;
+				StreamUtils.CopyStream(outstream, defilteredstream, 0x30);
+
+				ushort last = 0;
+				while (outstream.Position < outstream.Length) {
+					ushort diff = outstream.ReadUInt16(EndianUtils.Endianness.LittleEndian);
+					ushort curr = (ushort)(last - diff);
+					defilteredstream.WriteUInt16(curr, EndianUtils.Endianness.LittleEndian);
+					last = curr;
+				}
+
+				if (!SevenZip.CRC.VerifyDigest(crc, defilteredstream.CopyToByteArray(), 0, (uint)defilteredstream.Length)) {
+					throw new Exception("crc mismatch");
+				}
+
+				defilteredstream.Position = 0;
+				return defilteredstream;
+			}
+
+			if (filter == 2) {
+				uint crc = instream.ReadUInt32(EndianUtils.Endianness.LittleEndian);
+				byte[] properties = instream.ReadBytes(5);
+				long length = instream.ReadInt64(EndianUtils.Endianness.LittleEndian);
+				Stream outstream = new MemoryStream((int)length);
+
+				SevenZip.Compression.LZMA.Decoder decoder = new SevenZip.Compression.LZMA.Decoder();
+				decoder.SetDecoderProperties(properties);
+				long compressedSize = instream.Length - instream.Position;
+				decoder.Code(instream, outstream, compressedSize, length, null);
+
+				MemoryStream defilteredstream = new MemoryStream((int)outstream.Length);
+				outstream.Position = 0;
+				StreamUtils.CopyStream(outstream, defilteredstream, 0x30);
+
+				uint last = 0;
+				while (outstream.Position < outstream.Length) {
+					uint diff = outstream.ReadUInt32(EndianUtils.Endianness.LittleEndian);
+					uint curr = (uint)(last - diff);
+					defilteredstream.WriteUInt32(curr, EndianUtils.Endianness.LittleEndian);
+					last = curr;
+				}
+
+				if (!SevenZip.CRC.VerifyDigest(crc, defilteredstream.CopyToByteArray(), 0, (uint)defilteredstream.Length)) {
+					throw new Exception("crc mismatch");
+				}
+
+				defilteredstream.Position = 0;
+				return defilteredstream;
+			}
+
+			if (filter == 3) {
+				uint crc = instream.ReadUInt32(EndianUtils.Endianness.LittleEndian);
+				byte[] properties = instream.ReadBytes(5);
+				long length = instream.ReadInt64(EndianUtils.Endianness.LittleEndian);
+				Stream outstream = new MemoryStream((int)length);
+
+				SevenZip.Compression.LZMA.Decoder decoder = new SevenZip.Compression.LZMA.Decoder();
+				decoder.SetDecoderProperties(properties);
+				long compressedSize = instream.Length - instream.Position;
+				decoder.Code(instream, outstream, compressedSize, length, null);
+
+				MemoryStream defilteredstream = new MemoryStream((int)outstream.Length);
+				outstream.Position = 0;
+				StreamUtils.CopyStream(outstream, defilteredstream, 0x30);
+
+				long pos = defilteredstream.Position;
+				ushort last = 0;
+				while (defilteredstream.Position < outstream.Length) {
+					ushort diff = outstream.ReadUInt16(EndianUtils.Endianness.LittleEndian);
+					ushort curr = (ushort)(last - diff);
+					defilteredstream.WriteUInt16(curr, EndianUtils.Endianness.LittleEndian);
+					defilteredstream.WriteUInt16(0, EndianUtils.Endianness.LittleEndian);
+					last = curr;
+				}
+				defilteredstream.Position = pos;
+				last = 0;
+				while (defilteredstream.Position < outstream.Length) {
+					ushort diff = outstream.ReadUInt16(EndianUtils.Endianness.LittleEndian);
+					ushort curr = (ushort)(last - diff);
+					defilteredstream.Position = defilteredstream.Position + 2;
+					defilteredstream.WriteUInt16(curr, EndianUtils.Endianness.LittleEndian);
+					last = curr;
+				}
+
+				if (!SevenZip.CRC.VerifyDigest(crc, defilteredstream.CopyToByteArray(), 0, (uint)defilteredstream.Length)) {
+					throw new Exception("crc mismatch");
+				}
+
+				defilteredstream.Position = 0;
+				return defilteredstream;
+			}
+
+			throw new Exception("unimplemented filter");
+		}
+	}
+}
---
+++ SenLib/FileFromScratchBase.cs
@@ -0,0 +1,64 @@
+﻿using HyoutaUtils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SenLib {
+	public abstract class FileFromScratchBase : FileFix {
+		public bool TryApply(string basepath, string backuppath) {
+			string subtargetpath = GetSubTargetPath();
+			string sha1 = GetTargetSha1();
+			string targetpath = Path.Combine(basepath, subtargetpath);
+
+			if (File.Exists(targetpath)) {
+				using (var ms = new FileStream(targetpath, FileMode.Open, FileAccess.Read, FileShare.Read).CopyToMemoryAndDispose()) {
+					ms.Position = 0;
+					if (SenUtils.CalcSha1(ms) == sha1) {
+						// target already has the correct file, nothing to do
+						return true;
+					}
+				}
+			}
+
+			// file doesn't exist or mismatches
+			Stream stream = DoCreate();
+			if (SenUtils.CalcSha1(stream) != sha1) {
+				throw new Exception("Created incorrect file.");
+			}
+			return SenUtils.TryWriteFileIfDifferent(stream, targetpath);
+		}
+
+		public bool TryRevert(string basepath, string backuppath) {
+			string subtargetpath = GetSubTargetPath();
+			string sha1 = GetTargetSha1();
+			string targetpath = Path.Combine(basepath, subtargetpath);
+
+			if (File.Exists(targetpath)) {
+				bool dodelete = false;
+				using (var ms = new FileStream(targetpath, FileMode.Open, FileAccess.Read, FileShare.Read).CopyToMemoryAndDispose()) {
+					ms.Position = 0;
+					if (SenUtils.CalcSha1(ms) == sha1) {
+						dodelete = true;
+					}
+				}
+
+				if (dodelete) {
+					return SenUtils.TryDeleteFile(targetpath);
+				}
+
+				return false;
+			}
+
+			return true;
+		}
+
+		public abstract string GetSubTargetPath();
+
+		public abstract string GetTargetSha1();
+
+		protected abstract Stream DoCreate();
+	}
+}
