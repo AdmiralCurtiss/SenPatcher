5f0ce5feb9a9ffcaaef371604b9e6e53f614f514
Admiral H. Curtiss <pikachu025@gmail.com>
2021-01-03 18:59:05 +0100

    Autodetect CS1 or 2 from CLI.

--- SenPatcherCli/Program.cs
+++ SenPatcherCli/Program.cs
@@ -43,17 +43,23 @@ namespace SenPatcherCli {
 
 			string path = args[0];
 			if (!Directory.Exists(path)) {
-				Console.WriteLine("No directory found at " + path + ".");
+				Console.WriteLine($"No directory found at {path}.");
 				return -1;
 			}
 
-
-			// TODO: detect whether we're Sen1 or 2
-			int sengame = 2;
+			int sengame;
+			if (File.Exists(System.IO.Path.Combine(path, "Sen1Launcher.exe"))) {
+				sengame = 1;
+			} else if (File.Exists(System.IO.Path.Combine(path, "Sen2Launcher.exe"))) {
+				sengame = 2;
+			} else {
+				Console.WriteLine($"Failed to detect whether {path} is CS1 or 2.");
+				return -1;
+			}
 
 			FileStorage storage = FileModExec.InitializeAndPersistFileStorage(path, sengame == 1 ? Sen1KnownFiles.Files : Sen2KnownFiles.Files);
 			if (storage == null) {
-				Console.WriteLine("Failed to initialize file storage from " + path + ".");
+				Console.WriteLine($"Failed to initialize file storage from {path}.");
 				return -1;
 			}
 
@@ -82,79 +88,12 @@ namespace SenPatcherCli {
 			}
 
 			if (!result.AllSuccessful) {
-				Console.WriteLine($"Failed to patch CS${sengame} at ${path}.");
+				Console.WriteLine($"Failed to patch CS{sengame} at {path}.");
 				return -1;
 			}
 
-			Console.WriteLine($"Successfully patched CS${sengame} at ${path}.");
+			Console.WriteLine($"Successfully patched CS{sengame} at {path}.");
 			return 0;
-
-			/*
-			if (!File.Exists(path)) {
-				Console.WriteLine("No file found at " + path + ".");
-				return -1;
-			}
-
-			Stream binary;
-			SenVersion? actualVersion;
-			try {
-				(binary, actualVersion) = SenVersionIdentifier.OpenAndIdentifyGame(path);
-			} catch (Exception ex) {
-				Console.WriteLine("Error while identifying " + path + ": " + ex.ToString());
-				return -1;
-			}
-
-			if (binary == null || actualVersion == null) {
-				Console.WriteLine("Could not identify file at " + path + " as any supported Cold Steel executable.");
-				return -1;
-			}
-
-			switch (actualVersion) {
-				case SenVersion.Sen1_v1_6_Jp:
-				case SenVersion.Sen1_v1_6_En: {
-					bool success = new Sen1PatchExec(path, binary, actualVersion.Value).ApplyPatches(
-						removeTurboSkip: true,
-						allowR2NotebookShortcut: true,
-						turboKey: 0xA,
-						fixTextureIds: true,
-						patchAssets: true
-					).AllSuccessful;
-
-					if (success) {
-						Console.WriteLine("Successfully patched CS1 at " + path + ".");
-					} else {
-						Console.WriteLine("Failed to patch CS1 at " + path + ".");
-						return -1;
-					}
-					break;
-				}
-				case SenVersion.Sen2_v1_4_1_Jp:
-				case SenVersion.Sen2_v1_4_1_En:
-				case SenVersion.Sen2_v1_4_2_Jp:
-				case SenVersion.Sen2_v1_4_2_En: {
-					bool success = new Sen2PatchExec(path, binary, actualVersion.Value).ApplyPatches(
-						removeTurboSkip: true,
-						patchAudioThread: true,
-						audioThreadDivisor: 1000,
-						patchBgmQueueing: true,
-						patchAssets: true
-					).AllSuccessful;
-
-					if (success) {
-						Console.WriteLine("Successfully patched CS2 at " + path + ".");
-					} else {
-						Console.WriteLine("Failed to patch CS2 at " + path + ".");
-						return -1;
-					}
-					break;
-				}
-				default:
-					Console.WriteLine("File at " + path + " was recognized but is not supported.");
-					return -1;
-			}
-
-			return 0;
-			*/
 		}
 	}
 }
