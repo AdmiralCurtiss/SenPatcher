26db018ec733db9509419156a0dc25cb0bd8500c
Admiral H. Curtiss <pikachu025@gmail.com>
2021-01-17 23:54:17 +0100

    Revert CS2 to v1.4.1 if that was the input.

--- SenLib/Sen2/FileFixes/ed8_2_exe.cs
+++ SenLib/Sen2/FileFixes/ed8_2_exe.cs
@@ -58,6 +58,14 @@ namespace SenLib.Sen2.FileFixes {
 		}
 
 		public IEnumerable<FileModResult> TryRevert(FileStorage storage) {
+			// if we have both v1.4.1s revert to that, since that likely means that was the original input
+			var us141 = storage.TryGetDuplicate(new HyoutaUtils.Checksum.SHA1(0xd5c333b4cd517d43ul, 0xe3868e159fbec37dul, 0xba4122d6u));
+			var jp141 = storage.TryGetDuplicate(new HyoutaUtils.Checksum.SHA1(0xb8158fb59e43c02eul, 0x904f813150d84133ul, 0x6d1a13e5u));
+			if (us141 != null && jp141 != null) {
+				return new FileModResult[] { new FileModResult(IsJp ? "bin/Win32/ed8_2_PC_JP.exe" : "bin/Win32/ed8_2_PC_US.exe", IsJp ? jp141 : us141) };
+			}
+
+			// if we don't, v1.4.2 definitely was the input, so revert to that
 			var s = storage.TryGetDuplicate(GetExecutableHash());
 			if (s == null) {
 				return null;
