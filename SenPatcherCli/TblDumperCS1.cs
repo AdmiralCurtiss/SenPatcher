135ca8a3f4b184942b23159f55f64ffd6e2d16e3
Admiral H. Curtiss <pikachu025@gmail.com>
2021-11-14 06:29:50 +0100

    Mostly decoded CS1 item layout.

--- SenPatcherCli/TblDumperCS1.cs
+++ SenPatcherCli/TblDumperCS1.cs
@@ -45,37 +45,59 @@ namespace SenPatcherCli.Sen1 {
 							stream = new DuplicatableByteArrayStream(tbl.BaseTbl.Entries[i].Data);
 							List<string> postprint = new List<string>();
 							sb.AppendFormat(" Idx {0:x4}", stream.ReadUInt16(e));
-							sb.AppendFormat(" {0:x4}", stream.ReadUInt16(e));
+							sb.AppendFormat(" Usable by {0:x4}", stream.ReadUInt16(e));
 							postprint.Add(stream.ReadNulltermString(encoding).Replace("\n", "{n}"));
 							sb.Append("\n");
-							sb.AppendFormat(" {0:x2}", stream.ReadUInt8());
-							sb.AppendFormat(" {0:x2}", stream.ReadUInt8());
-							sb.AppendFormat(" {0:x2}", stream.ReadUInt8());
-							sb.AppendFormat(" {0:x2}", stream.ReadUInt8());
-							sb.AppendFormat(" {0:x2}", stream.ReadUInt8());
-							sb.AppendFormat(" {0:x2}", stream.ReadUInt8());
-							sb.AppendFormat(" {0:x4}", stream.ReadUInt16(e));
-							sb.AppendFormat(" {0:x4}", stream.ReadUInt16(e));
-							sb.AppendFormat(" {0:x2}", stream.ReadUInt8());
-							sb.AppendFormat(" {0:x4}", stream.ReadUInt16(e));
-							sb.AppendFormat(" {0:x4}", stream.ReadUInt16(e));
+							byte itemType = stream.ReadUInt8();
+							sb.AppendFormat(" Type {0:x2}", itemType);
+							sb.AppendFormat(" | Element {0:x2}", stream.ReadUInt8());
+							sb.AppendFormat(" | Status/Breaker {0:x2}", stream.ReadUInt8()); // 1 == status quartz, 2 == breaker quartz, probably used to limit the equip-one-per-line
+							sb.AppendFormat(" | RNG+{0,3}", stream.ReadUInt8());
+							sb.AppendFormat(" | AttackArea {0,3}", stream.ReadUInt8()); // AoE of physical attack
 							sb.Append("\n");
-							sb.AppendFormat(" {0:x4}", stream.ReadUInt16(e));
-							sb.AppendFormat(" {0:x4}", stream.ReadUInt16(e));
-							sb.AppendFormat(" {0:x4}", stream.ReadUInt16(e));
-							sb.AppendFormat(" {0:x4}", stream.ReadUInt16(e));
-							sb.AppendFormat(" {0:x4}", stream.ReadUInt16(e));
-							sb.AppendFormat(" {0:x4}", stream.ReadUInt16(e));
-							sb.AppendFormat(" {0:x4}", stream.ReadUInt16(e));
-							sb.AppendFormat(" {0:x4}", stream.ReadUInt16(e));
-							sb.AppendFormat(" {0:x4}", stream.ReadUInt16(e));
+							if (itemType == 0xaa) {
+								byte passiveEffect = stream.ReadUInt8(); // eg. status ailment or breaker stat
+								ushort art2 = stream.ReadUInt16(e);
+								ushort art3 = stream.ReadUInt16(e);
+								byte rarity = stream.ReadUInt8(); // 0 = N, 1 == R, 2 = SR
+								ushort art1 = stream.ReadUInt16(e);
+								ushort procChance = stream.ReadUInt16(e); // for status/breaker
+								sb.AppendFormat(" Art1 {0,5}", art1);
+								sb.AppendFormat(" | Art2 {0,5}", art2);
+								sb.AppendFormat(" | Art3 {0,5}", art3);
+								sb.Append("\n");
+								sb.AppendFormat(" Rarity  {0:x2}", rarity);
+								sb.AppendFormat(" | Passive {0:x2}", passiveEffect);
+								sb.AppendFormat(" | Chance {0,3}", procChance);
+								sb.Append("\n");
+							} else {
+								sb.AppendFormat(" Action {0:x2}", stream.ReadUInt8());
+								sb.AppendFormat(" | Value1 {0,5}", stream.ReadUInt16(e));
+								sb.AppendFormat(" | Value2 {0,5}", stream.ReadUInt16(e));
+								sb.Append("\n");
+								sb.AppendFormat(" Action {0:x2}", stream.ReadUInt8());
+								sb.AppendFormat(" | Value1 {0,5}", stream.ReadUInt16(e));
+								sb.AppendFormat(" | Value2 {0,5}", stream.ReadUInt16(e));
+								sb.Append("\n");
+							}
+							sb.AppendFormat(" STR+{0,5} ", stream.ReadUInt16(e));
+							sb.AppendFormat(" | DEF+{0,5} ", stream.ReadUInt16(e));
+							sb.AppendFormat(" | ATS+{0,5} ", stream.ReadUInt16(e));
+							sb.AppendFormat(" | ADF+{0,5} ", stream.ReadUInt16(e));
 							sb.Append("\n");
-							sb.AppendFormat(" {0:x4}", stream.ReadUInt16(e));
-							sb.AppendFormat(" {0:x8}", stream.ReadUInt32(e));
-							sb.AppendFormat(" {0:x2}", stream.ReadUInt8());
-							sb.AppendFormat(" {0:x4}", stream.ReadUInt16(e));
-							sb.AppendFormat(" {0:x4}", stream.ReadUInt16(e));
-							sb.AppendFormat(" {0:x4}", stream.ReadUInt16(e));
+							sb.AppendFormat(" ACC+{0,5}%", stream.ReadUInt16(e));
+							sb.AppendFormat(" | EVA+{0,5}%", stream.ReadUInt16(e));
+							sb.AppendFormat(" | SPD+{0,5} ", stream.ReadUInt16(e));
+							sb.AppendFormat(" | MOV+{0,5} ", stream.ReadUInt16(e));
+							sb.Append("\n");
+							sb.AppendFormat(" HP+ {0,5} ", stream.ReadUInt16(e));
+							sb.AppendFormat(" | EP+ {0,5} ", stream.ReadUInt16(e));
+							sb.Append("\n");
+							sb.AppendFormat(" Mira {0,6}", stream.ReadUInt32(e));
+							sb.AppendFormat(" | CarryLimit {0,3}", stream.ReadUInt8()); // max you're allowed to have
+							sb.AppendFormat(" | SortOrder {0,5}", stream.ReadUInt16(e)); // within the type
+							sb.AppendFormat(" | ? {0,5}", stream.ReadUInt16(e)); // only shows up on quartzes, no idea what this is though
+							sb.AppendFormat(" | ? {0:x4}", stream.ReadUInt16(e)); // DLC related? or maybe costume related?
 							postprint.Add(stream.ReadNulltermString(encoding).Replace("\n", "{n}"));
 							postprint.Add(stream.ReadNulltermString(encoding).Replace("\n", "{n}"));
 							sb.Append("\n");
@@ -87,7 +109,7 @@ namespace SenPatcherCli.Sen1 {
 							foreach (string s in postprint) {
 								sb.AppendFormat("\n{0}", s);
 							}
-							sb.Append("\n\n");
+							sb.Append("\n\n===========================================\n\n");
 							break;
 						}
 					case TblType.magic: {
