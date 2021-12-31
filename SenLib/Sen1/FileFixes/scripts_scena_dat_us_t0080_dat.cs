e3b98da97d2a58d8d2a648de87aeba758607e304
Admiral H. Curtiss <pikachu025@gmail.com>
2021-12-31 22:56:07 +0100

    CS1 Bonding event formatting fixes: Ch1 Evening Towa (#22)

--- SenLib/Sen1/FileFixes/scripts_scena_dat_us_t0080_dat.cs
+++ SenLib/Sen1/FileFixes/scripts_scena_dat_us_t0080_dat.cs
@@ -5,7 +5,7 @@ using System.IO;
 namespace SenLib.Sen1.FileFixes {
 	public class scripts_scena_dat_us_t0080_dat : FileMod {
 		public string GetDescription() {
-			return "Fix double space in Student Union building (chess club).";
+			return "Fix minor text errors in Student Union building.";
 		}
 
 		public IEnumerable<FileModResult> TryApply(FileStorage storage) {
@@ -15,8 +15,18 @@ namespace SenLib.Sen1.FileFixes {
 			}
 			MemoryStream bin = s.CopyToMemoryAndDispose();
 
+			// double space
 			var patcher = new SenScriptPatcher(bin);
 			patcher.RemovePartialCommand(0x1f278, 0x88, 0x1f2df, 1);
+
+			// formatting issues in Towa Chapter 1 Evening bonding event
+			bin.SwapBytes(0x31889, 0x3188f);
+			bin.SwapBytes(0x31ac0, 0x31ac2);
+			bin.SwapBytes(0x31b09, 0x31b0e);
+			bin.SwapBytes(0x31b2d, 0x31b35);
+			bin.SwapBytes(0x3205c, 0x32066);
+			bin.SwapBytes(0x320a5, 0x320ab);
+			bin.SwapBytes(0x32175, 0x32179);
 
 			return new FileModResult[] { new FileModResult("data/scripts/scena/dat_us/t0080.dat", bin) };
 		}
