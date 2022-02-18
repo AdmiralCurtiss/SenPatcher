71663d07b19bfaba75371c2598b59eaed3fb3342
Admiral H. Curtiss <pikachu025@gmail.com>
2022-02-18 09:01:05 +0100

    CS2: Arts Support fixes so it renders correctly at AR != 16/9. (#9)

--- SenLib/Sen2/Sen2ExecutablePatches.ArtsSupportCutin.cs
+++ SenLib/Sen2/Sen2ExecutablePatches.ArtsSupportCutin.cs
@@ -20,6 +20,7 @@ namespace SenLib.Sen2 {
 			long flagOffInjectPos = jp ? 0x468091 : 0x468251;
 			long flagOnInjectPos = jp ? 0x433650 : 0x433aa0;
 			long texcoordInjectPos = jp ? 0x4a22a9 : 0x4a25b9;
+			long configStructPos = jp ? 0x10f7cf4 : 0x10fc574;
 			long addressFlag = state.AddressOfScriptCompilerFlag;
 
 			// turn on flag when arts support starts
@@ -81,7 +82,7 @@ namespace SenLib.Sen2 {
 				bin.WriteUInt8(0x90); // nop
 				jumpBack.SetTarget(state.Mapper.MapRomToRam((ulong)bin.Position));
 
-				long newRegionStartRam = state.Region50a.Address;
+				long newRegionStartRam = state.RegionScriptCompilerFunction1.Address;
 				long newRegionStartRom = state.Mapper.MapRamToRom(newRegionStartRam);
 				bin.Position = newRegionStartRom;
 				jumpToNewCode.SetTarget((ulong)newRegionStartRam);
@@ -95,19 +96,38 @@ namespace SenLib.Sen2 {
 				skip_modification.WriteJump(0x75);     // jne skip_modification
 
 				bin.WriteUInt24(0xc60201, be);         // mov byte ptr[edx],1
+
+				bin.WriteUInt8(0xba);                  // mov edx,configStructPos
+				bin.WriteInt32((int)(configStructPos));
+				bin.WriteUInt16(0x8b12, be);           // mov edx,dword ptr[edx]
+				bin.WriteUInt16(0x85d2, be);           // test edx,edx
+				skip_modification.WriteJump(0x74);     // jz skip_modification
+				bin.WriteUInt24(0x8b4210, be);         // eax,dword ptr[edx+10h] ; width
+				bin.WriteUInt24(0x8b5214, be);         // edx,dword ptr[edx+14h] ; height
+				bin.WriteUInt32(0x660f6ec0, be);       // movd xmm0,eax
+				bin.WriteUInt24(0x0f5bc0, be);         // cvtdq2ps xmm0,xmm0
+				bin.WriteUInt32(0x660f6eca, be);       // movd xmm1,edx
+				bin.WriteUInt24(0x0f5bc9, be);         // cvtdq2ps xmm1,xmm1
+				bin.WriteUInt32(0xf30f5ec1, be);       // divss xmm0,xmm1 ; xmm0 now holds the actual aspect ratio
+				bin.WriteUInt40(0xba398ee33f, be);     // mov edx,float(16/9)
+				bin.WriteUInt32(0x660f6eca, be);       // movd xmm1,edx
+				bin.WriteUInt32(0xf30f5ec8, be);       // divss xmm1,xmm0 ; xmm1 now holds (16/9) / (real AR), ie the factor to multiply the horizontal texcoords with
+
 				bin.WriteUInt40(0xb8cdcc4c3d, be);     // mov eax,float(0.05)
 				bin.WriteUInt32(0x660f6ec0, be);       // movd xmm0,eax
-				bin.WriteUInt40(0xb80000803f, be);     // mov eax,float(1.0)
-				bin.WriteUInt32(0x660f6ec8, be);       // movd xmm1,eax
+				bin.WriteUInt32(0xf30f59c1, be);       // mulss xmm0,xmm1
 				bin.WriteUInt40(0xb8cdcc4c3e, be);     // mov eax,float(0.2)
 				bin.WriteUInt32(0x660f6ed0, be);       // movd xmm2,eax
+				bin.WriteUInt32(0xf30f59d1, be);       // mulss xmm2,xmm1
+				bin.WriteUInt40(0xb80000803f, be);     // mov eax,float(1.0)
+				bin.WriteUInt32(0x660f6ec8, be);       // movd xmm1,eax
 
 				skip_modification.SetTarget(state.Mapper.MapRomToRam((ulong)bin.Position));
 
 				bin.WriteUInt48(instr);
 				jumpBack.WriteJump5Byte(0xe9);   // jmp jumpBack
 
-				state.Region50a.TakeToAddress(state.Mapper.MapRomToRam(bin.Position), "Arts Support cutin fixes: Modify texcoords");
+				state.RegionScriptCompilerFunction1.TakeToAddress(state.Mapper.MapRomToRam(bin.Position), "Arts Support cutin fixes: Modify texcoords");
 			}
 
 			// modify matrix during arts support
@@ -124,7 +144,7 @@ namespace SenLib.Sen2 {
 				jumpToNewCode.WriteJump5Byte(0xe9); // jmp jumpToNewCode
 				jumpBack.SetTarget(state.Mapper.MapRomToRam((ulong)bin.Position));
 
-				long newRegionStartRam = state.RegionScriptCompilerFunction23.Address;
+				long newRegionStartRam = state.RegionScriptCompilerFunction1.Address;
 				long newRegionStartRom = state.Mapper.MapRamToRom(newRegionStartRam);
 				bin.Position = newRegionStartRom;
 				jumpToNewCode.SetTarget((ulong)newRegionStartRam);
@@ -150,13 +170,37 @@ namespace SenLib.Sen2 {
 				bin.WriteUInt32(0xf30f58c1, be);       // addss xmm0,xmm1
 				bin.WriteUInt40(0xf30f114024, be);     // movss dword ptr[eax+24h],xmm0
 
+				// if aspect ratio != 16:9, shift camera so that model is still at left edge of texture
+				// this is pretty silly tbqh but oh well
+				bin.WriteUInt8(0xba);                  // mov edx,configStructPos
+				bin.WriteInt32((int)(configStructPos));
+				bin.WriteUInt16(0x8b12, be);           // mov edx,dword ptr[edx]
+				bin.WriteUInt16(0x85d2, be);           // test edx,edx
+				skip_modification.WriteJump(0x74);     // jz skip_modification
+				bin.WriteUInt24(0x8b4a10, be);         // ecx,dword ptr[edx+10h] ; width
+				bin.WriteUInt24(0x8b5214, be);         // edx,dword ptr[edx+14h] ; height
+				bin.WriteUInt32(0x660f6ec1, be);       // movd xmm0,ecx
+				bin.WriteUInt24(0x0f5bc0, be);         // cvtdq2ps xmm0,xmm0
+				bin.WriteUInt32(0x660f6eca, be);       // movd xmm1,edx
+				bin.WriteUInt24(0x0f5bc9, be);         // cvtdq2ps xmm1,xmm1
+				bin.WriteUInt32(0xf30f5ec1, be);       // divss xmm0,xmm1 ; xmm0 now holds the actual aspect ratio
+				bin.WriteUInt40(0xba398ee33f, be);     // mov edx,float(16/9)
+				bin.WriteUInt32(0x660f6eca, be);       // movd xmm1,edx
+				bin.WriteUInt32(0xf30f5cc1, be);       // subss xmm0,xmm1
+				bin.WriteUInt40(0xba3333f3be, be);     // mov edx,float(-0.475)
+				bin.WriteUInt32(0x660f6eca, be);       // movd xmm1,edx
+				bin.WriteUInt32(0xf30f59c1, be);       // mulss xmm0,xmm1
+				bin.WriteUInt40(0xf30f104820, be);     // movss xmm1,dword ptr[eax+20h]
+				bin.WriteUInt32(0xf30f58c8, be);       // addss xmm1,xmm0
+				bin.WriteUInt40(0xf30f114820, be);     // movss dword ptr[eax+20h],xmm1
+
 				skip_modification.SetTarget(state.Mapper.MapRomToRam((ulong)bin.Position));
 
 				bin.WriteUInt32(instr1);
 				bin.WriteUInt8(instr3);
 				jumpBack.WriteJump5Byte(0xe9);   // jmp jumpBack
 
-				state.RegionScriptCompilerFunction23.TakeToAddress(state.Mapper.MapRomToRam(bin.Position), "Arts Support cutin fixes: Modify matrix");
+				state.RegionScriptCompilerFunction1.TakeToAddress(state.Mapper.MapRomToRam(bin.Position), "Arts Support cutin fixes: Modify matrix");
 			}
 		}
 	}
