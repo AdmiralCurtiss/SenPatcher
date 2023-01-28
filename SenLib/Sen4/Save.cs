aaa81acf904fd44e2796ca33c8b9447d2d838f0c
Admiral H. Curtiss <pikachu025@gmail.com>
2023-01-28 18:01:39 +0100

    CS4: Add janky PS4 -> PC save conversion code.

--- SenLib/Sen4/Save.cs
+++ SenLib/Sen4/Save.cs
@@ -44,5 +44,41 @@ namespace SenLib.Sen4 {
 
 			return true;
 		}
+
+		private static void Skip4(Stream input) {
+			uint v = input.ReadUInt32();
+			if (v != 0) {
+				Console.WriteLine("Warning: trashed nonzero uint {0:x8} at {1:x8}", v, input.Position - 4);
+			}
+		}
+
+		public static Stream ConvertPs4ToPc(Stream input) {
+			// this is complete guesswork based on comparing a PC and PS4 save saved immediately after hitting New Game
+			// might break everything
+			MemoryStream output = new MemoryStream();
+			StreamUtils.CopyStream(input, output, 8);
+			input.ReadUInt32(); // old filesize
+			output.WriteUInt32(1463976, EndianUtils.Endianness.LittleEndian);
+			StreamUtils.CopyStream(input, output, 0x152dc - 12);
+			Skip4(input);
+			StreamUtils.CopyStream(input, output, 0xc8);
+			for (int i = 0; i < 144; ++i) {
+				Skip4(input);
+				StreamUtils.CopyStream(input, output, 0x18);
+				Skip4(input);
+				Skip4(input);
+				Skip4(input);
+				StreamUtils.CopyStream(input, output, 0x3a8);
+			}
+			StreamUtils.CopyStream(input, output, 0x7be20 - 0x36fa4);
+			Skip4(input);
+			Skip4(input);
+			Skip4(input);
+			StreamUtils.CopyStream(input, output, input.Length - input.Position - 0x8);
+
+			output.Position = 0;
+			FixSaveChecksum(output);
+			return output;
+		}
 	}
 }
