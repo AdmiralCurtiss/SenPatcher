84bff9a2201a0b6c66356e50366863d1601f10f4
Admiral H. Curtiss <pikachu025@gmail.com>
2023-06-25 03:09:48 +0200

    Add lipflap monitoring to CS2 native playground.

--- SenPatcherCli/PlaygroundCS2.cpp
+++ SenPatcherCli/PlaygroundCS2.cpp
@@ -150,6 +150,26 @@ static void __fastcall log_ui_script_execution_end(uint32_t opcode, script_exec_
     log_script_execution_end(2 | 0x80, opcode, data);
 }
 
+namespace {
+struct lipflap_thisstruct {
+    char* lipflapdata;
+};
+struct lipflap_stackstruct {
+    uint32_t edx;
+    uint32_t ecx;
+    uint32_t unknown;
+    uint32_t param1;
+    uint32_t param2;
+};
+} // namespace
+
+static void __fastcall log_lipflaps(lipflap_thisstruct* thisptr, lipflap_stackstruct* stackptr) {
+    std::string s = fmt::format(
+        "edx {0:08x}, ecx {1:08x}, unknown {2:08x}, param1 {3:08}, param2 {4:08}\n", stackptr->edx,
+        stackptr->ecx, stackptr->unknown, stackptr->param1, stackptr->param2);
+    OutputDebugStringA(s.c_str());
+}
+
 static void inject_script_exec(char*& current_free_offset, void* inject_loc, void* startfunc,
                                void* endfunc) {
     {
@@ -232,6 +252,45 @@ static void inject_script_exec(char*& current_free_offset, void* inject_loc, voi
     }
 }
 
+static void inject_lipflap_monitor(char*& current_free_offset, void* func) {
+    // the function at 0x5780e0 is called for lipflap info
+    // this has no outgoing function calls, so we can completely copy & paste it elsewhere and
+    // hijack the function itself. we also know eax is safe to change here.
+
+    void* inject_func = reinterpret_cast<void*>(0x5780e0);
+    size_t inject_func_len = 0x578134 - 0x5780e0;
+
+    PageUnprotect unprotect(inject_func, inject_func_len);
+    void* copied_function = current_free_offset;
+    memcpy(current_free_offset, inject_func, inject_func_len);
+    current_free_offset += inject_func_len;
+    memset(inject_func, 0xcc, inject_func_len);
+
+    char* inject_func_replacement = reinterpret_cast<char*>(inject_func);
+    WriteInstruction({(char)0x51}, inject_func_replacement);             // push ecx
+    WriteInstruction({(char)0x52}, inject_func_replacement);             // push edx
+    WriteInstruction({(char)0x89, (char)0xe2}, inject_func_replacement); // mov edx,esp
+
+    // mov eax,func
+    WriteInstruction({(char)0xb8}, inject_func_replacement);
+    uint32_t addr1 = reinterpret_cast<uint32_t>(func);
+    memcpy(inject_func_replacement, &addr1, 4);
+    inject_func_replacement += 4;
+
+    WriteInstruction({(char)0xff, (char)0xd0}, inject_func_replacement); // call eax
+
+    WriteInstruction({(char)0x5a}, inject_func_replacement); // pop edx
+    WriteInstruction({(char)0x59}, inject_func_replacement); // pop ecx
+
+    // mov eax,copied_function
+    WriteInstruction({(char)0xb8}, inject_func_replacement);
+    uint32_t addr2 = reinterpret_cast<uint32_t>(copied_function);
+    memcpy(inject_func_replacement, &addr2, 4);
+    inject_func_replacement += 4;
+
+    WriteInstruction({(char)0xff, (char)0xe0}, inject_func_replacement); // jmp eax
+}
+
 static PDirectInput8Create addr_PDirectInput8Create = 0;
 static void* SetupHacks() {
     addr_PDirectInput8Create = LoadForwarderAddress();
@@ -242,12 +301,13 @@ static void* SetupHacks() {
     }
 
     char* current_free_offset = reinterpret_cast<char*>(new_page);
-    inject_script_exec(current_free_offset, reinterpret_cast<void*>(0x005e0e20),
-                       log_ed8_script_execution_start, log_ed8_script_execution_end);
-    inject_script_exec(current_free_offset, reinterpret_cast<void*>(0x005e0e40),
-                       log_scenario_script_execution_start, log_scenario_script_execution_end);
-    inject_script_exec(current_free_offset, reinterpret_cast<void*>(0x00632790),
-                       log_ui_script_execution_start, log_ui_script_execution_end);
+    inject_lipflap_monitor(current_free_offset, log_lipflaps);
+    // inject_script_exec(current_free_offset, reinterpret_cast<void*>(0x005e0e20),
+    //                    log_ed8_script_execution_start, log_ed8_script_execution_end);
+    // inject_script_exec(current_free_offset, reinterpret_cast<void*>(0x005e0e40),
+    //                    log_scenario_script_execution_start, log_scenario_script_execution_end);
+    // inject_script_exec(current_free_offset, reinterpret_cast<void*>(0x00632790),
+    //                    log_ui_script_execution_start, log_ui_script_execution_end);
 
     // mark newly allocated page as executable
     {
