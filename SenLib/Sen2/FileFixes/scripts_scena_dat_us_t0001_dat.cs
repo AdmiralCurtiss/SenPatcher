aa47c6afdf31fb931ef8f0bc0c801af5544ddc20
Admiral H. Curtiss <pikachu025@gmail.com>
2023-12-10 17:06:57 +0100

    CS2: More tweaks in NG+ scene. (#76)

--- SenLib/Sen2/FileFixes/scripts_scena_dat_us_t0001_dat.cs
+++ SenLib/Sen2/FileFixes/scripts_scena_dat_us_t0001_dat.cs
@@ -22,7 +22,12 @@ namespace SenLib.Sen2.FileFixes {
 			var patcher = new SenScriptPatcher(bin);
 
 			// What -> Wh-What
-			patcher.ExtendPartialCommand(0x18ada, 0x6f, 0x18ae9, new byte[] { 0x2d, 0x57, 0x68 });
+			// Actually on further inspection the stutter isn't really audible in game, so let's not.
+			// patcher.ExtendPartialCommand(0x18ada, 0x6f, 0x18ae9, new byte[] { 0x2d, 0x57, 0x68 });
+
+			// Remove clip on Rosine's last '...', PS4 does this and it *is* kinda out of place and also duplicated from earlier in the scene.
+			patcher.RemovePartialCommand(0x1b2e3, 0x12, 0x1b2e6, 5);
+
 
 			return new FileModResult[] { new FileModResult("data/scripts/scena/dat_us/t0001.dat", bin) };
 		}
