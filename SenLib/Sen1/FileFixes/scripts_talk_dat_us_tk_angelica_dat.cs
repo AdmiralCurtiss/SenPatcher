452d4ab03c4db88be75a30ad90929e206f7a1181
Admiral H. Curtiss <pikachu025@gmail.com>
2024-02-15 02:53:47 +0100

    CS1: Two files got swapped somehow...

--- SenLib/Sen1/FileFixes/scripts_talk_dat_us_tk_angelica_dat.cs
+++ SenLib/Sen1/FileFixes/scripts_talk_dat_us_tk_angelica_dat.cs
@@ -7,31 +7,33 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SenLib.Sen1.FileFixes {
-	class scripts_talk_dat_us_tk_becky_dat : FileMod {
+	class scripts_talk_dat_us_tk_angelica_dat : FileMod {
 		public string GetDescription() {
-			return "Fix typo in conversation with Becky.";
+			return "Fix extra word in conversation with Angelica.";
 		}
 
 		public IEnumerable<FileModResult> TryApply(FileStorage storage) {
-			var file = storage.TryGetDuplicate(new HyoutaUtils.Checksum.SHA1(0x3eb462def36cc151ul, 0x03e48657aa18b6b0ul, 0x2abcd830u));
+			var file = storage.TryGetDuplicate(new HyoutaUtils.Checksum.SHA1(0xe5f61f3108f8e1d4ul, 0x3ca539b93bac91cful, 0xe37b709au));
 			if (file == null) {
 				return null;
 			}
 
 			var bin = file.CopyToMemory();
+			var patcher = new SenScriptPatcher(bin);
 
-			bin.Position = 0x13e;
-			bin.WriteByte(0x61);
+			// remove extra 'to'
+			// this should be right but I can't find this line in-game, so let's disable it for now...
+			//patcher.RemovePartialCommand(0x619, 0x8f, 0x676, 0x3);
 
-			return new FileModResult[] { new FileModResult("data/scripts/talk/dat_us/tk_becky.dat", bin) };
+			return new FileModResult[] { new FileModResult("data/scripts/talk/dat_us/tk_angelica.dat", bin) };
 		}
 
 		public IEnumerable<FileModResult> TryRevert(FileStorage storage) {
-			var file = storage.TryGetDuplicate(new HyoutaUtils.Checksum.SHA1(0x3eb462def36cc151ul, 0x03e48657aa18b6b0ul, 0x2abcd830u));
+			var file = storage.TryGetDuplicate(new HyoutaUtils.Checksum.SHA1(0xe5f61f3108f8e1d4ul, 0x3ca539b93bac91cful, 0xe37b709au));
 			if (file == null) {
 				return null;
 			}
-			return new FileModResult[] { new FileModResult("data/scripts/talk/dat_us/tk_becky.dat", file) };
+			return new FileModResult[] { new FileModResult("data/scripts/talk/dat_us/tk_angelica.dat", file) };
 		}
 	}
 }
