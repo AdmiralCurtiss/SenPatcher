3acc33860af6ead2607e5af22613921bf15fac1c
Admiral H. Curtiss <pikachu025@gmail.com>
2024-03-09 23:28:34 +0100

    Port CS3 AllowSwitchToNightmare patch to native.

--- native/main_cs3.cpp
+++ native/main_cs3.cpp
@@ -1331,7 +1331,28 @@ static void AllowSwitchToNightmare(SenPatcher::Logger& logger,
                                    char* textRegion,
                                    GameVersion version,
                                    char*& codespace,
-                                   char* codespaceEnd) {}
+                                   char* codespaceEnd) {
+    using namespace SenPatcher::x64;
+    char* setMaxDifficultyIndexPos =
+        GetCodeAddressJpEn(version, textRegion, 0x14042a667, 0x140436347) + 1;
+    char* switchFromNightmareWarning =
+        GetCodeAddressJpEn(version, textRegion, 0x140236118, 0x14023c65d) + 1;
+
+    // allow nightmare to be selected when difficulty is currently not nightmare
+    {
+        PageUnprotect page(logger, setMaxDifficultyIndexPos, 4);
+        const uint32_t value = 5;
+        std::memcpy(setMaxDifficultyIndexPos, &value, 4);
+    }
+
+    // don't show warning when trying to switch away from nightmare
+    {
+        PageUnprotect page(logger, switchFromNightmareWarning, 4);
+        const uint32_t value = 0x1e4;
+        std::memcpy(switchFromNightmareWarning, &value, 4);
+    }
+}
+
 static void SwapBrokenMasterQuartzValuesForDisplay(SenPatcher::Logger& logger,
                                                    char* textRegion,
                                                    GameVersion version,
