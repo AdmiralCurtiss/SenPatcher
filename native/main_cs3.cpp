65babd7a37e5db83d5848ec5b02b82714b7a0bb2
Admiral H. Curtiss <pikachu025@gmail.com>
2024-03-10 18:35:49 +0100

    Port CS3 DisablePauseOnFocusLoss patch to native.

--- native/main_cs3.cpp
+++ native/main_cs3.cpp
@@ -1547,8 +1547,119 @@ static void PatchShowMouseCursor(SenPatcher::Logger& logger,
 static void PatchDisablePauseOnFocusLoss(SenPatcher::Logger& logger,
                                          char* textRegion,
                                          GameVersion version,
-                                         char*& codespace,
-                                         char* codespaceEnd) {}
+                                         char*& codespace_,
+                                         char* codespaceEnd_) {
+    using namespace SenPatcher::x64;
+    char* silenceAudioIfUnfocusedPos1 =
+        GetCodeAddressJpEn(version, textRegion, 0x1400f943b, 0x1400f9abb);
+    char* silenceAudioIfUnfocusedPos2 =
+        GetCodeAddressJpEn(version, textRegion, 0x1400f9474, 0x1400f9af4);
+    char* runMainGameLoopIfUnfocusedPos =
+        GetCodeAddressJpEn(version, textRegion, 0x1400f7024, 0x1400f76a4);
+    char* skipMouseButtonsIfUnfocusedPos1 =
+        GetCodeAddressJpEn(version, textRegion, 0x14012dfd2, 0x140131552);
+    char* skipMouseButtonsIfUnfocusedPos2 =
+        GetCodeAddressJpEn(version, textRegion, 0x141d43de0, 0x141d59f80);
+
+    // don't silence audio output when unfocused
+    int distance = silenceAudioIfUnfocusedPos2 - silenceAudioIfUnfocusedPos1;
+    PageUnprotect page(logger, silenceAudioIfUnfocusedPos1, distance);
+    {
+        char* tmp = silenceAudioIfUnfocusedPos1;
+        BranchHelper1Byte branch;
+        branch.SetTarget(silenceAudioIfUnfocusedPos2);
+        branch.WriteJump(tmp, JumpCondition::JMP);
+
+        // the rest of this we reserve for the mouse click asm below
+        // (there's not enough space there to write a 12byte absolute jmp)
+        for (int i = 2; i < distance; ++i) {
+            *tmp++ = 0xcc; // int 3
+        }
+    }
+
+    // still run main game loop when unfocused
+    {
+        char* tmp = runMainGameLoopIfUnfocusedPos;
+        PageUnprotect page(logger, tmp, 6);
+        for (size_t i = 0; i < 6; ++i) {
+            *tmp++ = 0x90; // nop
+        }
+    }
+
+    const auto write_instruction_32 = [&](char*& codepos, uint32_t instr) {
+        *codepos++ = (char)((instr >> 24) & 0xff);
+        *codepos++ = (char)((instr >> 16) & 0xff);
+        *codepos++ = (char)((instr >> 8) & 0xff);
+        *codepos++ = (char)(instr & 0xff);
+    };
+    const auto write_instruction_24 = [&](char*& codepos, uint32_t instr) {
+        *codepos++ = (char)((instr >> 16) & 0xff);
+        *codepos++ = (char)((instr >> 8) & 0xff);
+        *codepos++ = (char)(instr & 0xff);
+    };
+    const auto write_instruction_16 = [&](char*& codepos, uint32_t instr) {
+        *codepos++ = (char)((instr >> 8) & 0xff);
+        *codepos++ = (char)(instr & 0xff);
+    };
+
+    // avoid processing mouse clicks when unfocused
+    // (this previously happened only implicitly because the game didn't run...)
+    // assemble some logic to skip mouse button processing when unfocused
+    {
+        char* codespaceStart = (silenceAudioIfUnfocusedPos1) + 2;
+        char* codespaceEnd = silenceAudioIfUnfocusedPos2;
+        char* codespace = codespaceStart;
+
+        BranchHelper4Byte jump_to_codespace;
+        jump_to_codespace.SetTarget(codespace);
+        BranchHelper4Byte back_to_function;
+        BranchHelper1Byte back_to_function_short;
+        BranchHelper1Byte skip_processing;
+
+        back_to_function.SetTarget(skipMouseButtonsIfUnfocusedPos1 + 6);
+        int32_t relativeGetKeyStateAddress;
+        std::memcpy(&relativeGetKeyStateAddress, skipMouseButtonsIfUnfocusedPos1 + 2, 4);
+        char* GetKeyStateAddress =
+            relativeGetKeyStateAddress + (skipMouseButtonsIfUnfocusedPos1 + 6);
+        char* GameStateAddress = skipMouseButtonsIfUnfocusedPos2;
+
+        using JC = JumpCondition;
+        {
+            char* tmp = skipMouseButtonsIfUnfocusedPos1;
+            PageUnprotect page(logger, tmp, 6);
+            jump_to_codespace.WriteJump(tmp, JC::JMP); // jmp jump_to_codespace
+            *tmp = 0x90;                               // nop
+        }
+
+        const auto WriteRelativeAddress32 = [](char*& codepos, char* absoluteTarget) {
+            char* absoluteHere = codepos + 4;
+            int32_t a = static_cast<int32_t>(absoluteTarget - absoluteHere);
+            std::memcpy(codepos, &a, 4);
+            codepos += 4;
+        };
+
+        write_instruction_24(codespace, 0x488d05u); // lea rax,[address_that_holds_game_state]
+        WriteRelativeAddress32(codespace, GameStateAddress);
+        write_instruction_24(codespace, 0x488b00u);   // mov rax,[rax]
+        write_instruction_24(codespace, 0x4885c0u);   // test rax,rax
+        skip_processing.WriteJump(codespace, JC::JZ); // jz skip_processing
+        write_instruction_24(codespace, 0x0fb680u);   // movzx eax,byte ptr[rax+1ce0h]
+        write_instruction_32(codespace, 0xe01c0000u);
+        write_instruction_16(codespace, 0x85c0);      // test eax,eax
+        skip_processing.WriteJump(codespace, JC::JZ); // jz skip_processing
+        write_instruction_24(codespace, 0x488d05u);   // lea rax,[GetKeyState]
+        WriteRelativeAddress32(codespace, GetKeyStateAddress);
+        write_instruction_24(codespace, 0x488b00u);           // mov rax,[rax]
+        write_instruction_24(codespace, 0x4885c0u);           // test rax,rax
+        skip_processing.WriteJump(codespace, JC::JZ);         // jz skip_processing
+        write_instruction_16(codespace, 0xffd0);              // call rax
+        back_to_function_short.WriteJump(codespace, JC::JMP); // jmp back_to_function_short
+        skip_processing.SetTarget(codespace);
+        write_instruction_24(codespace, 0x6633c0); // xor ax,ax
+        back_to_function_short.SetTarget(codespace);
+        back_to_function.WriteJump(codespace, JC::JMP); // jmp back_to_function
+    }
+}
 
 static void PatchForceXInput(SenPatcher::Logger& logger,
                              char* textRegion,
