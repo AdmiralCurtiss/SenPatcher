3d5c713bd3cac7fd98ad781cce3dd1c2866ad7f3
Admiral H. Curtiss <pikachu025@gmail.com>
2024-03-13 22:43:16 +0100

    Move CS3 modloader injection logic out of main_cs3.cpp

--- native/main_cs3.cpp
+++ native/main_cs3.cpp
@@ -24,6 +24,7 @@
 
 #include "sen3/exe_patch.h"
 #include "sen3/file_fixes.h"
+#include "sen3/inject_modloader.h"
 
 #include "p3a/p3a.h"
 #include "p3a/structs.h"
@@ -448,239 +449,6 @@ static void* __fastcall FSoundOpenForwarder(FSoundFile* soundFile, const char* p
     return nullptr;
 }
 
-static void InjectAtFFileOpen(SenPatcher::Logger& logger,
-                              char* textRegion,
-                              GameVersion version,
-                              char*& codespace,
-                              char* codespaceEnd) {
-    using namespace SenPatcher::x64;
-
-    char* const entryPoint = textRegion
-                             + (version == GameVersion::Japanese ? (0x1400f5270 - 0x140001000)
-                                                                 : (0x1400f58f0 - 0x140001000));
-    char* const exitPoint = textRegion
-                            + (version == GameVersion::Japanese ? (0x1400f538d - 0x140001000)
-                                                                : (0x1400f5a0d - 0x140001000));
-
-
-    char* codespaceBegin = codespace;
-    auto injectResult = InjectJumpIntoCode<12>(logger, entryPoint, R64::RAX, codespaceBegin);
-    char* inject = injectResult.JumpBackAddress;
-    const auto& overwrittenInstructions = injectResult.OverwrittenInstructions;
-
-    std::memcpy(codespace, overwrittenInstructions.data(), overwrittenInstructions.size());
-    codespace += overwrittenInstructions.size();
-
-    Emit_PUSH_R64(codespace, R64::RAX);
-    Emit_PUSH_R64(codespace, R64::RCX);
-    Emit_PUSH_R64(codespace, R64::RDX);
-    Emit_PUSH_R64(codespace, R64::R8);
-
-    // for some reason I cannot figure out, the caller has to reserve stack space for the called
-    // function's parameters in x64???
-    Emit_SUB_R64_IMM32(codespace, R64::RSP, 0x20);
-
-    Emit_MOV_R64_IMM64(codespace, R64::RAX, std::bit_cast<uint64_t>(&FFileOpenForwarder));
-    Emit_CALL_R64(codespace, R64::RAX);
-
-    Emit_ADD_R64_IMM32(codespace, R64::RSP, 0x20);
-
-    Emit_TEST_R64_R64(codespace, R64::RAX, R64::RAX);
-    Emit_POP_R64(codespace, R64::R8);
-    Emit_POP_R64(codespace, R64::RDX);
-    Emit_POP_R64(codespace, R64::RCX);
-    Emit_POP_R64(codespace, R64::RAX);
-
-    // if successful, skip the following jmp r9
-    BranchHelper1Byte success;
-    success.WriteJump(codespace, JumpCondition::JNZ);
-
-    // go back to function and pretend nothing happened
-    Emit_MOV_R64_IMM64(codespace, R64::R9, std::bit_cast<uint64_t>(inject));
-    Emit_JMP_R64(codespace, R64::R9);
-
-    // return that we succeeded
-    success.SetTarget(codespace);
-    Emit_MOV_R64_IMM64(codespace, R64::RAX, 1);
-
-    // jump to exit point
-    Emit_MOV_R64_IMM64(codespace, R64::RCX, std::bit_cast<uint64_t>(exitPoint));
-    Emit_JMP_R64(codespace, R64::RCX);
-}
-
-static void InjectAtFFileGetFilesize(SenPatcher::Logger& logger,
-                                     char* textRegion,
-                                     GameVersion version,
-                                     char*& codespace,
-                                     char* codespaceEnd) {
-    using namespace SenPatcher::x64;
-
-    char* const entryPoint = textRegion
-                             + (version == GameVersion::Japanese ? (0x1400f5120 - 0x140001000)
-                                                                 : (0x1400f57a0 - 0x140001000));
-    char* const exitPoint = textRegion
-                            + (version == GameVersion::Japanese ? (0x1400f5224 - 0x140001000)
-                                                                : (0x1400f58a4 - 0x140001000));
-
-
-    char* codespaceBegin = codespace;
-    auto injectResult = InjectJumpIntoCode<12>(logger, entryPoint, R64::RAX, codespaceBegin);
-    char* inject = injectResult.JumpBackAddress;
-    const auto& overwrittenInstructions = injectResult.OverwrittenInstructions;
-
-    std::memcpy(codespace, overwrittenInstructions.data(), overwrittenInstructions.size());
-    codespace += overwrittenInstructions.size();
-
-    Emit_PUSH_R64(codespace, R64::RCX);
-    Emit_PUSH_R64(codespace, R64::RDX);
-    Emit_PUSH_R64(codespace, R64::R8);
-    Emit_PUSH_R64(codespace, R64::R9);
-    Emit_PUSH_R64(codespace, R64::R10);
-    Emit_PUSH_R64(codespace, R64::R11);
-    Emit_SUB_R64_IMM32(codespace, R64::RSP, 0x20);
-
-    Emit_MOV_R64_IMM64(codespace, R64::RAX, std::bit_cast<uint64_t>(&FFileGetFilesizeForwarder));
-    Emit_CALL_R64(codespace, R64::RAX);
-
-    Emit_ADD_R64_IMM32(codespace, R64::RSP, 0x20);
-    Emit_POP_R64(codespace, R64::R11);
-    Emit_POP_R64(codespace, R64::R10);
-    Emit_POP_R64(codespace, R64::R9);
-    Emit_POP_R64(codespace, R64::R8);
-    Emit_POP_R64(codespace, R64::RDX);
-    Emit_POP_R64(codespace, R64::RCX);
-
-    // check for success
-    Emit_TEST_R64_R64(codespace, R64::RAX, R64::RAX);
-    BranchHelper1Byte success;
-    success.WriteJump(codespace, JumpCondition::JNZ);
-
-    // on fail, go back to function and pretend nothing happened
-    Emit_MOV_R64_IMM64(codespace, R64::RAX, std::bit_cast<uint64_t>(inject));
-    Emit_JMP_R64(codespace, R64::RAX);
-
-    // on success, return that we succeeded
-    success.SetTarget(codespace);
-    Emit_MOV_R64_IMM64(codespace, R64::RAX, 1);
-
-    // jump to exit point
-    Emit_MOV_R64_IMM64(codespace, R64::RCX, std::bit_cast<uint64_t>(exitPoint));
-    Emit_JMP_R64(codespace, R64::RCX);
-}
-
-static void InjectAtFreestandingGetFilesize(SenPatcher::Logger& logger,
-                                            char* textRegion,
-                                            GameVersion version,
-                                            char*& codespace,
-                                            char* codespaceEnd) {
-    using namespace SenPatcher::x64;
-
-    char* const entryPoint = textRegion
-                             + (version == GameVersion::Japanese ? (0x1400f4905 - 0x140001000)
-                                                                 : (0x1400f4ea5 - 0x140001000));
-    char* const exitPoint = textRegion
-                            + (version == GameVersion::Japanese ? (0x1400f4a2e - 0x140001000)
-                                                                : (0x1400f4fce - 0x140001000));
-
-
-    char* codespaceBegin = codespace;
-    auto injectResult = InjectJumpIntoCode<12>(logger, entryPoint, R64::RAX, codespaceBegin);
-    char* inject = injectResult.JumpBackAddress;
-    const auto& overwrittenInstructions = injectResult.OverwrittenInstructions;
-
-    std::memcpy(codespace, overwrittenInstructions.data(), overwrittenInstructions.size());
-    codespace += overwrittenInstructions.size();
-
-    Emit_PUSH_R64(codespace, R64::RCX);
-    Emit_PUSH_R64(codespace, R64::RDX);
-    Emit_PUSH_R64(codespace, R64::R8);
-    Emit_PUSH_R64(codespace, R64::R9);
-    Emit_PUSH_R64(codespace, R64::R10);
-    Emit_PUSH_R64(codespace, R64::R11);
-    Emit_SUB_R64_IMM32(codespace, R64::RSP, 0x20);
-
-    Emit_MOV_R64_IMM64(
-        codespace, R64::RAX, std::bit_cast<uint64_t>(&FreestandingGetFilesizeForwarder));
-    Emit_CALL_R64(codespace, R64::RAX);
-
-    Emit_ADD_R64_IMM32(codespace, R64::RSP, 0x20);
-    Emit_POP_R64(codespace, R64::R11);
-    Emit_POP_R64(codespace, R64::R10);
-    Emit_POP_R64(codespace, R64::R9);
-    Emit_POP_R64(codespace, R64::R8);
-    Emit_POP_R64(codespace, R64::RDX);
-    Emit_POP_R64(codespace, R64::RCX);
-
-    // if successful, skip the following jmp r9
-    Emit_TEST_R64_R64(codespace, R64::RAX, R64::RAX);
-    BranchHelper1Byte success;
-    success.WriteJump(codespace, JumpCondition::JNZ);
-
-    // go back to function and pretend nothing happened
-    Emit_MOV_R64_IMM64(codespace, R64::RAX, std::bit_cast<uint64_t>(inject));
-    Emit_JMP_R64(codespace, R64::RAX);
-
-    // return that we succeeded
-    success.SetTarget(codespace);
-    Emit_MOV_R64_IMM64(codespace, R64::RAX, 1);
-
-    // jump to exit point
-    Emit_MOV_R64_IMM64(codespace, R64::RCX, std::bit_cast<uint64_t>(exitPoint));
-    Emit_JMP_R64(codespace, R64::RCX);
-}
-
-static void InjectAtOpenFSoundFile(SenPatcher::Logger& logger,
-                                   char* textRegion,
-                                   GameVersion version,
-                                   char*& codespace,
-                                   char* codespaceEnd) {
-    using namespace SenPatcher::x64;
-
-    char* const entryPoint = textRegion
-                             + (version == GameVersion::Japanese ? (0x140086900 - 0x140001000)
-                                                                 : (0x140086900 - 0x140001000));
-
-
-    char* codespaceBegin = codespace;
-    auto injectResult = InjectJumpIntoCode<15>(logger, entryPoint, R64::RAX, codespaceBegin);
-    char* inject = injectResult.JumpBackAddress;
-    const auto& overwrittenInstructions = injectResult.OverwrittenInstructions;
-
-    std::memcpy(codespace, overwrittenInstructions.data(), overwrittenInstructions.size());
-    codespace += overwrittenInstructions.size();
-
-    Emit_PUSH_R64(codespace, R64::RCX);
-    Emit_PUSH_R64(codespace, R64::RDX);
-    Emit_PUSH_R64(codespace, R64::R8);
-    Emit_PUSH_R64(codespace, R64::R9);
-    Emit_PUSH_R64(codespace, R64::R10);
-    Emit_PUSH_R64(codespace, R64::R11);
-    Emit_SUB_R64_IMM32(codespace, R64::RSP, 0x20);
-
-    Emit_MOV_R64_IMM64(codespace, R64::RAX, std::bit_cast<uint64_t>(&FSoundOpenForwarder));
-    Emit_CALL_R64(codespace, R64::RAX);
-
-    Emit_ADD_R64_IMM32(codespace, R64::RSP, 0x20);
-    Emit_POP_R64(codespace, R64::R11);
-    Emit_POP_R64(codespace, R64::R10);
-    Emit_POP_R64(codespace, R64::R9);
-    Emit_POP_R64(codespace, R64::R8);
-    Emit_POP_R64(codespace, R64::RDX);
-    Emit_POP_R64(codespace, R64::RCX);
-
-    Emit_TEST_R64_R64(codespace, R64::RAX, R64::RAX);
-    BranchHelper1Byte success;
-    success.WriteJump(codespace, JumpCondition::JNZ);
-
-    // on failure go back to function
-    Emit_MOV_R64_IMM64(codespace, R64::R9, std::bit_cast<uint64_t>(inject));
-    Emit_JMP_R64(codespace, R64::R9);
-
-    // on success just return from the function, RAX already has the correct return value
-    success.SetTarget(codespace);
-    Emit_RET(codespace);
-}
-
 static bool CaseInsensitiveEquals(std::string_view lhs, std::string_view rhs) {
     if (lhs.size() != rhs.size()) {
         return false;
@@ -791,11 +559,22 @@ static void* SetupHacks(SenPatcher::Logger& logger) {
 
     LoadModP3As(logger, s_LoadedModsData, baseDir);
 
-    InjectAtFFileOpen(logger, static_cast<char*>(codeBase), version, newPage, newPageEnd);
-    InjectAtFFileGetFilesize(logger, static_cast<char*>(codeBase), version, newPage, newPageEnd);
-    InjectAtFreestandingGetFilesize(
-        logger, static_cast<char*>(codeBase), version, newPage, newPageEnd);
-    InjectAtOpenFSoundFile(logger, static_cast<char*>(codeBase), version, newPage, newPageEnd);
+    SenLib::Sen3::InjectAtFFileOpen(
+        logger, static_cast<char*>(codeBase), version, newPage, newPageEnd, &FFileOpenForwarder);
+    SenLib::Sen3::InjectAtFFileGetFilesize(logger,
+                                           static_cast<char*>(codeBase),
+                                           version,
+                                           newPage,
+                                           newPageEnd,
+                                           &FFileGetFilesizeForwarder);
+    SenLib::Sen3::InjectAtFreestandingGetFilesize(logger,
+                                                  static_cast<char*>(codeBase),
+                                                  version,
+                                                  newPage,
+                                                  newPageEnd,
+                                                  &FreestandingGetFilesizeForwarder);
+    SenLib::Sen3::InjectAtOpenFSoundFile(
+        logger, static_cast<char*>(codeBase), version, newPage, newPageEnd, &FSoundOpenForwarder);
 
     DeglobalizeMutexes(logger, static_cast<char*>(codeBase), version, newPage, newPageEnd);
     AddSenPatcherVersionToTitle(logger, static_cast<char*>(codeBase), version, newPage, newPageEnd);
