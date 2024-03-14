173600ec84795e96a6f97f243b1a6774e2604ae1
Admiral H. Curtiss <pikachu025@gmail.com>
2024-03-14 23:21:24 +0100

    Fix warnings in x64/emitter.cpp

--- native/x64/emitter.cpp
+++ native/x64/emitter.cpp
@@ -5,7 +5,7 @@
 
 namespace SenPatcher::x64 {
 static void Emit_Instr3Byte_R64_R64(char*& address, R64 dst, R64 src, char instr) {
-    char prefix = 0x48;
+    int prefix = 0x48;
     if (src >= R64::R8) {
         prefix |= 0x01;
     }
@@ -13,162 +13,162 @@ static void Emit_Instr3Byte_R64_R64(char*& address, R64 dst, R64 src, char instr
         prefix |= 0x04;
     }
 
-    char op = 0xc0;
+    int op = 0xc0;
     op |= (static_cast<int>(src) & 0x7);
     op |= ((static_cast<int>(dst) & 0x7) << 3);
 
-    *address++ = prefix;
+    *address++ = static_cast<char>(prefix);
     *address++ = instr;
-    *address++ = op;
+    *address++ = static_cast<char>(op);
 }
 
 void Emit_MOV_R64_R64(char*& address, R64 dst, R64 src) {
-    Emit_Instr3Byte_R64_R64(address, dst, src, 0x8b);
+    Emit_Instr3Byte_R64_R64(address, dst, src, static_cast<char>(0x8b));
 }
 
 void Emit_MOV_R64_IMM64(char*& address, R64 dst, uint64_t imm, size_t desiredEncodingLength) {
     if (desiredEncodingLength == 0 && imm == 0) {
         Emit_XOR_R64_R64(address, dst, dst);
     } else if (desiredEncodingLength == 0 && imm < 0x1'0000'0000) {
-        char op = 0xb8;
+        int op = 0xb8;
         op |= (static_cast<int>(dst) & 0x7);
 
         if (dst >= R64::R8) {
-            *address++ = 0x41;
+            *address++ = static_cast<char>(0x41);
         }
-        *address++ = op;
+        *address++ = static_cast<char>(op);
         uint32_t imm32 = static_cast<uint32_t>(imm);
         std::memcpy(address, &imm32, 4);
         address += 4;
     } else if ((desiredEncodingLength == 0 || desiredEncodingLength == 7)
                && (imm < 0x8000'0000 || imm >= 0xffff'ffff'8000'0000)) {
-        char prefix = 0x48;
+        int prefix = 0x48;
         if (dst >= R64::R8) {
             prefix |= 0x01;
         }
 
-        char op = 0xc0;
+        int op = 0xc0;
         op |= (static_cast<int>(dst) & 0x7);
 
-        *address++ = prefix;
-        *address++ = 0xc7;
-        *address++ = op;
+        *address++ = static_cast<char>(prefix);
+        *address++ = static_cast<char>(0xc7);
+        *address++ = static_cast<char>(op);
         uint32_t imm32 = static_cast<uint32_t>(imm & 0xffff'ffff);
         std::memcpy(address, &imm32, 4);
         address += 4;
     } else {
-        char prefix = 0x48;
+        int prefix = 0x48;
         if (dst >= R64::R8) {
             prefix |= 0x01;
         }
 
-        char op = 0xb8;
+        int op = 0xb8;
         op |= (static_cast<int>(dst) & 0x7);
 
-        *address++ = prefix;
-        *address++ = op;
+        *address++ = static_cast<char>(prefix);
+        *address++ = static_cast<char>(op);
         std::memcpy(address, &imm, 8);
         address += 8;
     }
 }
 
 void Emit_JMP_R64(char*& address, R64 target) {
-    char op = 0xe0;
+    int op = 0xe0;
     op |= (static_cast<int>(target) & 0x7);
 
     if (target >= R64::R8) {
-        *address++ = 0x41;
+        *address++ = static_cast<char>(0x41);
     }
-    *address++ = 0xff;
-    *address++ = op;
+    *address++ = static_cast<char>(0xff);
+    *address++ = static_cast<char>(op);
 }
 
 void Emit_CALL_R64(char*& address, R64 target) {
-    char op = 0xd0;
+    int op = 0xd0;
     op |= (static_cast<int>(target) & 0x7);
 
     if (target >= R64::R8) {
-        *address++ = 0x41;
+        *address++ = static_cast<char>(0x41);
     }
-    *address++ = 0xff;
-    *address++ = op;
+    *address++ = static_cast<char>(0xff);
+    *address++ = static_cast<char>(op);
 }
 
 void Emit_RET(char*& address) {
-    *address++ = 0xc3;
+    *address++ = static_cast<char>(0xc3);
 }
 
 void Emit_PUSH_R64(char*& address, R64 reg) {
-    char op = 0x50;
+    int op = 0x50;
     op |= (static_cast<int>(reg) & 0x7);
 
     if (reg >= R64::R8) {
-        *address++ = 0x41;
+        *address++ = static_cast<char>(0x41);
     }
-    *address++ = op;
+    *address++ = static_cast<char>(op);
 }
 
 void Emit_POP_R64(char*& address, R64 reg) {
-    char op = 0x58;
+    int op = 0x58;
     op |= (static_cast<int>(reg) & 0x7);
 
     if (reg >= R64::R8) {
-        *address++ = 0x41;
+        *address++ = static_cast<char>(0x41);
     }
-    *address++ = op;
+    *address++ = static_cast<char>(op);
 }
 
 void Emit_ADD_R64_R64(char*& address, R64 dst, R64 src) {
-    Emit_Instr3Byte_R64_R64(address, dst, src, 0x03);
+    Emit_Instr3Byte_R64_R64(address, dst, src, static_cast<char>(0x03));
 }
 
 void Emit_ADC_R64_R64(char*& address, R64 dst, R64 src) {
-    Emit_Instr3Byte_R64_R64(address, dst, src, 0x13);
+    Emit_Instr3Byte_R64_R64(address, dst, src, static_cast<char>(0x13));
 }
 
 void Emit_SUB_R64_R64(char*& address, R64 dst, R64 src) {
-    Emit_Instr3Byte_R64_R64(address, dst, src, 0x2b);
+    Emit_Instr3Byte_R64_R64(address, dst, src, static_cast<char>(0x2b));
 }
 
 void Emit_TEST_R64_R64(char*& address, R64 dst, R64 src) {
-    Emit_Instr3Byte_R64_R64(address, dst, src, 0x85);
+    Emit_Instr3Byte_R64_R64(address, dst, src, static_cast<char>(0x85));
 }
 
 void Emit_CMP_R64_R64(char*& address, R64 dst, R64 src) {
-    Emit_Instr3Byte_R64_R64(address, dst, src, 0x3b);
+    Emit_Instr3Byte_R64_R64(address, dst, src, static_cast<char>(0x3b));
 }
 
 void Emit_AND_R64_R64(char*& address, R64 dst, R64 src) {
-    Emit_Instr3Byte_R64_R64(address, dst, src, 0x23);
+    Emit_Instr3Byte_R64_R64(address, dst, src, static_cast<char>(0x23));
 }
 
 void Emit_OR_R64_R64(char*& address, R64 dst, R64 src) {
-    Emit_Instr3Byte_R64_R64(address, dst, src, 0x0b);
+    Emit_Instr3Byte_R64_R64(address, dst, src, static_cast<char>(0x0b));
 }
 
 void Emit_XOR_R64_R64(char*& address, R64 dst, R64 src) {
     if (dst == src && dst < R64::R8) {
-        char op = 0xc0;
+        int op = 0xc0;
         op |= (static_cast<int>(src) & 0x7);
         op |= ((static_cast<int>(dst) & 0x7) << 3);
-        *address++ = 0x33;
-        *address++ = op;
+        *address++ = static_cast<char>(0x33);
+        *address++ = static_cast<char>(op);
     } else {
-        Emit_Instr3Byte_R64_R64(address, dst, src, 0x33);
+        Emit_Instr3Byte_R64_R64(address, dst, src, static_cast<char>(0x33));
     }
 }
 
 void Emit_ADD_R64_IMM32(char*& address, R64 dst, int32_t imm) {
     if (imm >= -128 && imm <= 127) {
-        Emit_Instr3Byte_R64_R64(address, R64::RAX, dst, 0x83);
+        Emit_Instr3Byte_R64_R64(address, R64::RAX, dst, static_cast<char>(0x83));
         *address++ = static_cast<char>(imm);
     } else if (dst == R64::RAX) {
-        *address++ = 0x48;
-        *address++ = 0x05;
+        *address++ = static_cast<char>(0x48);
+        *address++ = static_cast<char>(0x05);
         std::memcpy(address, &imm, 4);
         address += 4;
     } else {
-        Emit_Instr3Byte_R64_R64(address, R64::RAX, dst, 0x81);
+        Emit_Instr3Byte_R64_R64(address, R64::RAX, dst, static_cast<char>(0x81));
         std::memcpy(address, &imm, 4);
         address += 4;
     }
@@ -176,7 +176,7 @@ void Emit_ADD_R64_IMM32(char*& address, R64 dst, int32_t imm) {
 
 void Emit_ADC_R64_IMM32(char*& address, R64 dst, int32_t imm) {
     if (imm >= -128 && imm <= 127) {
-        Emit_Instr3Byte_R64_R64(address, R64::RDX, dst, 0x83);
+        Emit_Instr3Byte_R64_R64(address, R64::RDX, dst, static_cast<char>(0x83));
         *address++ = static_cast<char>(imm);
     } else if (dst == R64::RAX) {
         *address++ = 0x48;
@@ -184,7 +184,7 @@ void Emit_ADC_R64_IMM32(char*& address, R64 dst, int32_t imm) {
         std::memcpy(address, &imm, 4);
         address += 4;
     } else {
-        Emit_Instr3Byte_R64_R64(address, R64::RDX, dst, 0x81);
+        Emit_Instr3Byte_R64_R64(address, R64::RDX, dst, static_cast<char>(0x81));
         std::memcpy(address, &imm, 4);
         address += 4;
     }
@@ -192,15 +192,15 @@ void Emit_ADC_R64_IMM32(char*& address, R64 dst, int32_t imm) {
 
 void Emit_SUB_R64_IMM32(char*& address, R64 dst, int32_t imm) {
     if (imm >= -128 && imm <= 127) {
-        Emit_Instr3Byte_R64_R64(address, R64::RBP, dst, 0x83);
+        Emit_Instr3Byte_R64_R64(address, R64::RBP, dst, static_cast<char>(0x83));
         *address++ = static_cast<char>(imm);
     } else if (dst == R64::RAX) {
-        *address++ = 0x48;
-        *address++ = 0x2d;
+        *address++ = static_cast<char>(0x48);
+        *address++ = static_cast<char>(0x2d);
         std::memcpy(address, &imm, 4);
         address += 4;
     } else {
-        Emit_Instr3Byte_R64_R64(address, R64::RBP, dst, 0x81);
+        Emit_Instr3Byte_R64_R64(address, R64::RBP, dst, static_cast<char>(0x81));
         std::memcpy(address, &imm, 4);
         address += 4;
     }
@@ -234,12 +234,12 @@ void BranchHelper1Byte::Commit(char* source) {
 void BranchHelper1Byte::WriteJump(char*& address, JumpCondition condition) {
     assert(condition != JumpCondition::CALL);
     if (condition == JumpCondition::JMP) {
-        *address++ = 0xeb;
+        *address++ = static_cast<char>(0xeb);
     } else {
-        char op = 0x70 | static_cast<int>(condition);
+        char op = static_cast<char>(0x70 | static_cast<int>(condition));
         *address++ = op;
     }
-    *address++ = 0xfe; // self-jump until set
+    *address++ = static_cast<char>(0xfe); // self-jump until set
     AddSource(address - 1);
 }
 
@@ -272,14 +272,14 @@ void BranchHelper4Byte::Commit(char* source) {
 void BranchHelper4Byte::WriteJump(char*& address, JumpCondition condition) {
     int32_t offset;
     if (condition == JumpCondition::JMP) {
-        *address++ = 0xe9;
+        *address++ = static_cast<char>(0xe9);
         offset = -5;
     } else if (condition == JumpCondition::CALL) {
-        *address++ = 0xe8;
+        *address++ = static_cast<char>(0xe8);
         offset = -5;
     } else {
-        *address++ = 0x0f;
-        char op = 0x80 | static_cast<int>(condition);
+        *address++ = static_cast<char>(0x0f);
+        char op = static_cast<char>(0x80 | static_cast<int>(condition));
         *address++ = op;
         offset = -6;
     }
