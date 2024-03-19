5c137c275f713c56ec9747e7d2e6c8de845ea1e4
Admiral H. Curtiss <pikachu025@gmail.com>
2024-03-19 03:28:57 +0100

    Use bit_cast in PageUnprotect

--- native/x64/page_unprotect.cpp
+++ native/x64/page_unprotect.cpp
@@ -1,5 +1,7 @@
 #include "page_unprotect.h"
 
+#include <bit>
+
 #include "logger.h"
 
 #define WIN32_LEAN_AND_MEAN
@@ -9,7 +11,7 @@ namespace SenPatcher::x64 {
 PageUnprotect::PageUnprotect(SenPatcher::Logger& logger, void* addr, size_t length) : Log(logger) {
     // FIXME: check length/alignment, this might span multiple pages!
     Length = 0x1000;
-    Address = reinterpret_cast<void*>(reinterpret_cast<uint64_t>(addr) & (~(Length - 1)));
+    Address = std::bit_cast<void*>(std::bit_cast<uint64_t>(addr) & (~(Length - 1)));
     Log.Log("Unprotecting ").LogHex(Length).Log(" bytes at ").LogPtr(Address);
     if (VirtualProtect(Address, Length, PAGE_READWRITE, &Attributes)) {
         Log.Log(" -> Success, previous attributes were ").LogHex(Attributes).Log(".\n");
