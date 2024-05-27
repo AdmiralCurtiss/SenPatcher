aafaf0439dfc4a766859ad25e0204a250b95c675
Admiral H. Curtiss <pikachu025@gmail.com>
2024-05-27 20:00:40 +0200

    Allow packing PKA from multiple folders

--- native/sentools/pka_pack.cpp
+++ native/sentools/pka_pack.cpp
@@ -25,6 +25,39 @@
 #include "util/text.h"
 
 namespace SenTools {
+// in the vanilla games the pkgs are always uppercase except for the extension which is
+// lowercase, so make sure we pack like that too...
+static void NormalizePkgName(std::array<char, 0x20>& pkgName) {
+    const size_t length = [&]() {
+        for (size_t i = 0; i < pkgName.size(); ++i) {
+            if (pkgName[i] == '\0') {
+                return i;
+            }
+        }
+        return pkgName.size();
+    }();
+    const size_t extensionSeparator = [&]() {
+        for (size_t i = 0; i < length; ++i) {
+            if (pkgName[i] == '.') {
+                return i;
+            }
+        }
+        return length;
+    }();
+    for (size_t i = 0; i < extensionSeparator; ++i) {
+        char& c = pkgName[i];
+        if (c >= 'a' && c <= 'z') {
+            c = c + ('A' - 'a');
+        }
+    }
+    for (size_t i = extensionSeparator; i < length; ++i) {
+        char& c = pkgName[i];
+        if (c >= 'A' && c <= 'Z') {
+            c = c + ('a' - 'A');
+        }
+    }
+}
+
 int PKA_Pack_Function(int argc, char** argv) {
     using HyoutaUtils::EndianUtils::Endianness::LittleEndian;
 
@@ -39,9 +72,8 @@ int PKA_Pack_Function(int argc, char** argv) {
 
     const auto& options = parser.parse_args(argc, argv);
     const auto& args = parser.args();
-    if (args.size() != 1) {
-        parser.error(args.size() == 0 ? "No input directory given."
-                                      : "More than 1 input directory given.");
+    if (args.size() == 0) {
+        parser.error("No input directory given.");
         return -1;
     }
 
@@ -50,7 +82,6 @@ int PKA_Pack_Function(int argc, char** argv) {
         return -1;
     }
 
-    std::string_view source(args[0]);
     std::string_view target(options["output"]);
 
     // first collect info about every file in every pkg
@@ -66,56 +97,69 @@ int PKA_Pack_Function(int argc, char** argv) {
         HyoutaUtils::IO::File FileHandle;
         std::array<char, 0x20> PkgName;
         std::vector<PkgPackFile> Files;
+        bool IncludeInPka = true;
     };
     std::vector<PkgPackArchive> pkgPackFiles;
     {
-        std::filesystem::path rootDir(source);
-        std::error_code ec;
-        std::filesystem::directory_iterator iterator(rootDir, ec);
-        if (ec) {
-            return false;
-        }
-        for (auto const& entry : iterator) {
+        // returns true if we should continue processing, or false if we should exit
+        const auto add_pkg = [&](const std::filesystem::path& rootDir,
+                                 const std::filesystem::directory_entry& entry) -> bool {
+            std::error_code ec;
             if (entry.is_directory()) {
-                continue;
+                return true;
             }
             const auto ext = entry.path().extension().generic_u8string();
             if (!HyoutaUtils::TextUtils::CaseInsensitiveEquals(
                     ".pkg",
                     std::string_view(reinterpret_cast<const char*>(ext.data()), ext.size()))) {
-                continue;
+                return true;
             }
 
             const auto relativePath = std::filesystem::relative(entry.path(), rootDir, ec);
             if (relativePath.empty()) {
                 printf("Error while collecting files.\n");
-                return -1;
+                return false;
             }
             const auto filename = relativePath.u8string();
             const char8_t* filenameC = filename.c_str();
 
             std::array<char, 0x20> fn{};
-            for (size_t i = 0; i < fn.size(); ++i) {
+            for (size_t i = 0; i < fn.size() - 1; ++i) {
                 const char c = static_cast<char>(filenameC[i]);
                 if (c == '\0') {
                     break;
                 }
                 fn[i] = c;
             }
+            NormalizePkgName(fn);
+
+            auto existingArchive =
+                std::find_if(pkgPackFiles.begin(),
+                             pkgPackFiles.end(),
+                             [&](const PkgPackArchive& a) { return a.PkgName == fn; });
+            const bool archiveExistsAlready = (existingArchive != pkgPackFiles.end());
+            if (archiveExistsAlready) {
+                printf(
+                    "WARNING: %s exists multiple times, only the first parsed archive will be "
+                    "available in the PKA (but the contents of all instances will be packed).\n",
+                    fn.data());
+            }
+
             auto& fi = pkgPackFiles.emplace_back(PkgPackArchive{
                 .FileHandle = HyoutaUtils::IO::File(entry.path(), HyoutaUtils::IO::OpenMode::Read),
-                .PkgName = fn});
+                .PkgName = fn,
+                .IncludeInPka = !archiveExistsAlready});
 
             HyoutaUtils::IO::File& infile = fi.FileHandle;
             if (!infile.IsOpen()) {
                 printf("Failed opening pkg.\n");
-                return -1;
+                return false;
             }
             std::array<char, 8> pkgHeaderInitialBytes;
             if (infile.Read(pkgHeaderInitialBytes.data(), pkgHeaderInitialBytes.size())
                 != pkgHeaderInitialBytes.size()) {
                 printf("Failed to read pkg.\n");
-                return -1;
+                return false;
             }
             const uint32_t fileCountInPkg = HyoutaUtils::EndianUtils::FromEndian(
                 HyoutaUtils::MemRead::ReadUInt32(&pkgHeaderInitialBytes[4]), LittleEndian);
@@ -123,7 +167,7 @@ int PKA_Pack_Function(int argc, char** argv) {
             auto pkgHeaderData = std::make_unique_for_overwrite<char[]>(pkgHeaderLength);
             if (!pkgHeaderData) {
                 printf("Failed to allocate memory.\n");
-                return -1;
+                return false;
             }
             std::memcpy(
                 pkgHeaderData.get(), pkgHeaderInitialBytes.data(), pkgHeaderInitialBytes.size());
@@ -131,14 +175,14 @@ int PKA_Pack_Function(int argc, char** argv) {
                             pkgHeaderLength - pkgHeaderInitialBytes.size())
                 != (pkgHeaderLength - pkgHeaderInitialBytes.size())) {
                 printf("Failed to read pkg.\n");
-                return -1;
+                return false;
             }
 
             SenLib::PkgHeader pkgHeader;
             if (!SenLib::ReadPkgFromMemory(
                     pkgHeader, pkgHeaderData.get(), pkgHeaderLength, LittleEndian)) {
                 printf("Failed to read pkg header.\n");
-                return -1;
+                return false;
             }
 
             fi.Files.reserve(pkgHeader.FileCount);
@@ -147,15 +191,15 @@ int PKA_Pack_Function(int argc, char** argv) {
                 auto compressedData = std::make_unique_for_overwrite<char[]>(f.CompressedSize);
                 if (!compressedData) {
                     printf("Failed to allocate memory.\n");
-                    return -1;
+                    return false;
                 }
                 if (!infile.SetPosition(f.DataPosition)) {
                     printf("Failed to seek in pkg.\n");
-                    return -1;
+                    return false;
                 }
                 if (infile.Read(compressedData.get(), f.CompressedSize) != f.CompressedSize) {
                     printf("Failed to read pkg.\n");
-                    return -1;
+                    return false;
                 }
 
                 std::unique_ptr<char[]> dataBuffer;
@@ -163,7 +207,7 @@ int PKA_Pack_Function(int argc, char** argv) {
                 f.Data = compressedData.get();
                 if (!SenLib::ExtractAndDecompressPkgFile(dataBuffer, dataLength, f, LittleEndian)) {
                     printf("Failed to extract file from pkg.\n");
-                    return -1;
+                    return false;
                 }
                 fi.Files.emplace_back(PkgPackFile{
                     .Filename = f.Filename,
@@ -174,38 +218,19 @@ int PKA_Pack_Function(int argc, char** argv) {
                     .Flags = f.Flags,
                 });
             }
-        }
-    }
+        };
 
-    // in the vanilla games the pkgs are always uppercase except for the extension which is
-    // lowercase, so make sure we pack like that too...
-    for (PkgPackArchive& archive : pkgPackFiles) {
-        const size_t length = [&]() {
-            for (size_t i = 0; i < archive.PkgName.size(); ++i) {
-                if (archive.PkgName[i] == '\0') {
-                    return i;
+        for (size_t i = 0; i < args.size(); ++i) {
+            std::filesystem::path rootDir(args[i]);
+            std::error_code ec;
+            std::filesystem::directory_iterator iterator(rootDir, ec);
+            if (ec) {
+                return false;
+            }
+            for (const std::filesystem::directory_entry& entry : iterator) {
+                if (!add_pkg(rootDir, entry)) {
+                    return -1;
                 }
-            }
-            return archive.PkgName.size();
-        }();
-        const size_t extensionSeparator = [&]() {
-            for (size_t i = 0; i < length; ++i) {
-                if (archive.PkgName[i] == '.') {
-                    return i;
-                }
-            }
-            return length;
-        }();
-        for (size_t i = 0; i < extensionSeparator; ++i) {
-            char& c = archive.PkgName[i];
-            if (c >= 'a' && c <= 'z') {
-                c = c + ('A' - 'a');
-            }
-        }
-        for (size_t i = extensionSeparator; i < length; ++i) {
-            char& c = archive.PkgName[i];
-            if (c >= 'A' && c <= 'Z') {
-                c = c + ('a' - 'A');
             }
         }
     }
@@ -262,6 +287,10 @@ int PKA_Pack_Function(int argc, char** argv) {
     // calculate file offsets
     size_t pkaHeaderLength = (8u + 4u) + (filesByHash.size() * (0x20u + 8u + 4u + 4u + 4u));
     for (size_t i = 0; i < pkgPackFiles.size(); ++i) {
+        if (!pkgPackFiles[i].IncludeInPka) {
+            continue;
+        }
+
         pkaHeaderLength += (0x20u + 4u + ((0x40u + 0x20u) * pkgPackFiles[i].Files.size()));
     }
     uint64_t fileOffset = pkaHeaderLength;
@@ -295,6 +324,10 @@ int PKA_Pack_Function(int argc, char** argv) {
     WriteAdvUInt32(pkaHeaderWritePtr,
                    ToEndian(static_cast<uint32_t>(pkgPackFiles.size()), LittleEndian));
     for (size_t i = 0; i < pkgPackFiles.size(); ++i) {
+        if (!pkgPackFiles[i].IncludeInPka) {
+            continue;
+        }
+
         WriteAdvArray(pkaHeaderWritePtr, pkgPackFiles[i].PkgName);
         WriteAdvUInt32(pkaHeaderWritePtr,
                        ToEndian(static_cast<uint32_t>(pkgPackFiles[i].Files.size()), LittleEndian));
