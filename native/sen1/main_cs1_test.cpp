7e30b5437d66edd293b76b2fd851d7f156bc7f4a
Admiral H. Curtiss <pikachu025@gmail.com>
2024-07-27 20:50:52 +0200

    Fix building CS1 and CS2 asset fix builder tests

--- native/sen1/main_cs1_test.cpp
+++ native/sen1/main_cs1_test.cpp
@@ -1,10 +1,13 @@
 #include "sen1/file_fixes.h"
 
+#include "modload/loaded_mods.h"
+
 #include "util/logger.h"
 
 int main(int argc, char** argv) {
     HyoutaUtils::Logger logger;
+    SenLib::ModLoad::LoadedP3AData vanillaP3As;
     SenLib::Sen1::CreateAssetPatchIfNeeded(
-        logger, "f:\\SteamLibrary\\steamapps\\common\\Trails of Cold Steel");
+        logger, "f:\\SteamLibrary\\steamapps\\common\\Trails of Cold Steel", vanillaP3As);
     return 0;
 }
--- native/sen2/main_cs2_test.cpp
+++ native/sen2/main_cs2_test.cpp
@@ -1,10 +1,13 @@
 #include "sen2/file_fixes.h"
 
+#include "modload/loaded_mods.h"
+
 #include "util/logger.h"
 
 int main(int argc, char** argv) {
     HyoutaUtils::Logger logger;
+    SenLib::ModLoad::LoadedP3AData vanillaP3As;
     SenLib::Sen2::CreateAssetPatchIfNeeded(
-        logger, "f:\\SteamLibrary\\steamapps\\common\\Trails of Cold Steel II");
+        logger, "f:\\SteamLibrary\\steamapps\\common\\Trails of Cold Steel II", vanillaP3As);
     return 0;
 }
