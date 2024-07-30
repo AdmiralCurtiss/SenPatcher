9f6c8f149cb99a1b8faa9ec95ba42ac58ae0aac0
Admiral H. Curtiss <pikachu025@gmail.com>
2024-07-30 21:11:03 +0200

    Try to improve user experience of SenTools for Windows users that don't know what a command line application is

--- native/sentools/main_sentools.cpp
+++ native/sentools/main_sentools.cpp
@@ -17,6 +17,11 @@
 #include "pkg_pack.h"
 #include "pkg_repack.h"
 
+#ifdef _MSC_VER
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#endif
+
 namespace SenTools {
 static constexpr auto CliTools = {
     CliTool{.Name = P3A_Extract_Name,
@@ -57,10 +62,46 @@ static void PrintUsage() {
     }
 }
 
+#ifdef _MSC_VER
+static const char* StripPathToFilename(const char* path) {
+    const char* tmp = path;
+    const char* rv = path;
+    while (*tmp != '\0') {
+        if (*tmp == '/' || *tmp == '\\') {
+            rv = tmp + 1;
+        }
+        ++tmp;
+    }
+    return rv;
+}
+#endif
+
 int main(int argc, char** argv) {
     try {
         if (argc < 2) {
             PrintUsage();
+
+#ifdef _MSC_VER
+            // https://devblogs.microsoft.com/oldnewthing/20160125-00/?p=92922
+            // Check to see if the user launched this by double-clicking so we can tell them that
+            // this is a command line app -- you won't believe how many times I've seen someone be
+            // confused by the 'flashing window' in discord...
+            DWORD tmp;
+            if (GetConsoleProcessList(&tmp, 1) == 1) {
+                printf(
+                    "\n"
+                    "This is a command line application.\n"
+                    "To use this, open a command prompt or PowerShell and type the name of this "
+                    "executable (%s)\n"
+                    "followed by the name of the sub-tool you'd like to use.\n"
+                    "You will then get further instructions on what arguments are expected by the "
+                    "sub-tool.\n"
+                    "\n",
+                    (argc >= 1 ? StripPathToFilename(argv[0]) : "sentools.exe"));
+                system("pause");
+            }
+#endif
+
             return -1;
         }
 
