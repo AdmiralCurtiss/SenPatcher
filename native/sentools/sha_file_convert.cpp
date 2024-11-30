a1599d214e50cbf53a0b233ef948014e9b570d03
Admiral H. Curtiss <pikachu025@gmail.com>
2024-11-30 21:55:50 +0100

    Parse .sha file into tree structure

--- native/sentools/sha_file_convert.cpp
+++ native/sentools/sha_file_convert.cpp
@@ -1,9 +1,11 @@
 #include "sha_file_convert.h"
 
+#include <algorithm>
 #include <filesystem>
 #include <memory>
 #include <string>
 #include <string_view>
+#include <unordered_map>
 #include <vector>
 
 #include "cpp-optparse/OptionParser.h"
@@ -13,13 +15,90 @@
 #include "util/text.h"
 
 namespace {
-struct HashWithPath {
-    HyoutaUtils::Hash::SHA1 Hash;
-    std::string Path;
+struct TreeNode {
+    bool IsDirectory;
+    std::string Name;
+    HyoutaUtils::Hash::SHA1 Hash;                        // only if file
+    std::vector<TreeNode> Children;                      // only if directory
+    std::unordered_map<std::string, size_t> ChildLookup; // lookup for Children vector
 };
 } // namespace
 
 namespace SenTools {
+static std::string FilterPath(std::string_view in_path) {
+    std::string result;
+    size_t in = 0;
+
+    // drop leading path separators
+    while (in < in_path.size() && (in_path[in] == '/' || in_path[in] == '\\')) {
+        ++in;
+    }
+
+    while (in < in_path.size()) {
+        const char c = in_path[in];
+        if (c == '\0') {
+            break;
+        }
+        if (c == '\\' || c == '/') {
+            // collapse sequential path separators
+            result.push_back('/');
+            ++in;
+            while (in_path[in] == '\\' || in_path[in] == '/') {
+                ++in;
+            }
+        } else {
+            result.push_back(c);
+            ++in;
+        }
+    }
+
+    // drop trailing path separator
+    if (result.ends_with('/')) {
+        result.pop_back();
+    }
+
+    return result;
+}
+
+static bool Insert(TreeNode& tree, const HyoutaUtils::Hash::SHA1& hash, std::string_view path) {
+    // wasteful in terms of memory but whatever
+    auto slashpos = path.find_first_of('/');
+    if (slashpos != std::string_view::npos) {
+        std::string_view dirname = path.substr(0, slashpos);
+        std::string_view rest = path.substr(slashpos + 1);
+
+        auto dirnameLowercase = HyoutaUtils::TextUtils::ToLower(dirname);
+        auto existingDirIt = tree.ChildLookup.find(dirnameLowercase);
+        if (existingDirIt == tree.ChildLookup.end()) {
+            // no directory node yet, insert new
+            tree.ChildLookup.emplace(std::move(dirnameLowercase), tree.Children.size());
+            auto& newChild =
+                tree.Children.emplace_back(TreeNode{.IsDirectory = true,
+                                                    .Name = std::string(dirname),
+                                                    .Hash = HyoutaUtils::Hash::SHA1({})});
+            return Insert(newChild, hash, rest);
+        }
+        auto& child = tree.Children[existingDirIt->second];
+        if (!child.IsDirectory) {
+            return false;
+        }
+        return Insert(child, hash, rest);
+    }
+
+    // found directory to put this file in
+    auto pathLowercase = HyoutaUtils::TextUtils::ToLower(path);
+    auto existingFileIt = tree.ChildLookup.find(pathLowercase);
+    if (existingFileIt != tree.ChildLookup.end()) {
+        // file already exists
+        return false;
+    }
+
+    tree.ChildLookup.emplace(std::move(pathLowercase), tree.Children.size());
+    tree.Children.emplace_back(
+        TreeNode{.IsDirectory = false, .Name = std::string(path), .Hash = hash});
+    return true;
+}
+
 int SHA_File_Convert_Function(int argc, char** argv) {
     optparse::OptionParser parser;
     parser.description(SHA_File_Convert_ShortDescription);
@@ -47,7 +126,7 @@ int SHA_File_Convert_Function(int argc, char** argv) {
         target = tmp;
     }
 
-    std::vector<std::vector<HashWithPath>> entriesPerFile;
+    std::vector<TreeNode> treePerPath;
     std::filesystem::path targetpath(target.begin(), target.end());
     for (const auto& arg : args) {
         std::filesystem::path sourcepath(arg.begin(), arg.end());
@@ -71,7 +150,8 @@ int SHA_File_Convert_Function(int argc, char** argv) {
             return -1;
         }
 
-        std::vector<HashWithPath> entries;
+        TreeNode& root = treePerPath.emplace_back(
+            TreeNode{.IsDirectory = true, .Hash = HyoutaUtils::Hash::SHA1({})});
         std::string_view remaining(data.get(), *filesize);
 
         // skip UTF8 BOM if it's there
@@ -101,10 +181,14 @@ int SHA_File_Convert_Function(int argc, char** argv) {
                 return -1;
             }
 
-            entries.emplace_back(HashWithPath{.Hash = *hash, .Path = std::string(line.substr(42))});
-        }
+            auto path = FilterPath(line.substr(42));
+            if (path.empty()) {
+                printf("Invalid file format.\n");
+                return -1;
+            }
 
-        entriesPerFile.emplace_back(std::move(entries));
+            Insert(root, *hash, path);
+        }
     }
 
 
