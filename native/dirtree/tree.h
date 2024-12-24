6df5aa618cc8327f6da4e854aa8622a9cdaef5bb
Admiral H. Curtiss <pikachu025@gmail.com>
2024-12-24 13:52:30 +0100

    Add more metadata to DirTree::Tree struct

--- native/dirtree/tree.h
+++ native/dirtree/tree.h
@@ -8,6 +8,12 @@ struct Tree {
     const HyoutaUtils::DirTree::Entry* Entries = nullptr;
     size_t NumberOfEntries = 0;
     const char* StringTable = nullptr;
+    size_t StringTableLength = 0;
     const HyoutaUtils::Hash::SHA1* HashTable = nullptr;
+    size_t HashTableLength = 0;
+    const char* const* VersionNames = nullptr;
+    size_t NumberOfVersions = 0;
+    const char* const* DlcNames = nullptr;
+    size_t NumberOfDlcs = 0;
 };
 } // namespace HyoutaUtils::DirTree
