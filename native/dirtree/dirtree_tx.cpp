f26f343fa378e66e3d61f905d17cb404f3597909
Admiral H. Curtiss <pikachu025@gmail.com>
2024-12-24 15:18:14 +0100

    Regenerate TX dirtree in binary format with and with extra data

--- native/dirtree/dirtree_tx.cpp
+++ native/dirtree/dirtree_tx.cpp
@@ -2,11 +2,33 @@
 
 #include "generated/internal_dirtree_tx.h"
 
+#include "dirtree/entry.h"
+#include "dirtree/tree.h"
+#include "util/hash/sha1.h"
+
 namespace SenLib::TX {
+static constexpr const char* s_version_names[] = {
+    "Steam v1.00 (initial build)",
+    "Steam v1.00 (NG+ fix)",
+    "Steam v1.07",
+    "Steam v1.08",
+    "GOG v1.08",
+};
+
 HyoutaUtils::DirTree::Tree GetDirTree() {
-    return HyoutaUtils::DirTree::Tree{.Entries = s_dirtree.data(),
-                                      .NumberOfEntries = s_dirtree.size(),
-                                      .StringTable = s_stringtable.data(),
-                                      .HashTable = s_hashtable.data()};
+    return HyoutaUtils::DirTree::Tree{
+        .Entries = reinterpret_cast<const HyoutaUtils::DirTree::Entry*>(&s_raw_dirtree[0]),
+        .NumberOfEntries = s_raw_dirtree_entry_count,
+        .StringTable =
+            &s_raw_dirtree[s_raw_dirtree_entry_count * 16 + s_raw_dirtree_hash_count * 20],
+        .StringTableLength = s_raw_dirtree_string_size,
+        .HashTable = reinterpret_cast<const HyoutaUtils::Hash::SHA1*>(
+            &s_raw_dirtree[s_raw_dirtree_entry_count * 16]),
+        .HashTableLength = s_raw_dirtree_hash_count,
+        .VersionNames = s_version_names,
+        .NumberOfVersions = sizeof(s_version_names) / sizeof(s_version_names[0]),
+        .DlcNames = nullptr,
+        .NumberOfDlcs = s_max_dlc_index,
+    };
 }
 } // namespace SenLib::TX
