a5302072d15441ab9607bfc51e488983a64a9ce5
Admiral H. Curtiss <pikachu025@gmail.com>
2024-12-26 16:07:46 +0100

    Add fonts for imgui GUI

---
+++ native/sentools/gui_fonts.cpp
@@ -0,0 +1,40 @@
+#include "gui_fonts.h"
+
+#include "imgui.h"
+
+#include "gui_state.h"
+#include "sen/decompress_helper.h"
+
+namespace {
+static constexpr char CuprumFontData[] = {
+#include "cuprum.h"
+};
+static constexpr size_t CuprumFontLength = sizeof(CuprumFontData);
+static constexpr char NotoSansJpFontData[] = {
+#include "noto_sans_jp.h"
+};
+static constexpr size_t NotoSansJpFontLength = sizeof(NotoSansJpFontData);
+} // namespace
+
+namespace SenTools {
+void LoadFonts(ImGuiIO& io, GuiState& state) {
+    static std::vector<char> cuprum =
+        SenLib::DecompressFromBuffer(CuprumFontData, CuprumFontLength);
+    static std::vector<char> noto =
+        SenLib::DecompressFromBuffer(NotoSansJpFontData, NotoSansJpFontLength);
+
+    ImFontConfig config;
+    config.FontDataOwnedByAtlas = false;
+    io.Fonts->AddFontFromMemoryTTF(cuprum.data(),
+                                   static_cast<int>(cuprum.size()),
+                                   18.0f,
+                                   &config,
+                                   io.Fonts->GetGlyphRangesDefault());
+    config.MergeMode = true;
+    io.Fonts->AddFontFromMemoryTTF(noto.data(),
+                                   static_cast<int>(noto.size()),
+                                   20.0f,
+                                   &config,
+                                   io.Fonts->GetGlyphRangesJapanese());
+}
+} // namespace SenTools
---
+++ native/sentools/gui_fonts.h
@@ -0,0 +1,9 @@
+#pragma once
+
+struct ImGuiIO;
+
+namespace SenTools {
+struct GuiState;
+
+void LoadFonts(ImGuiIO& io, GuiState& state);
+} // namespace SenTools
