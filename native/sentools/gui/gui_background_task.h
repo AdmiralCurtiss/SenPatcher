2b5a1f63e9be6feab0920c50feeeaf773c4a0af7
Admiral H. Curtiss <pikachu025@gmail.com>
2025-05-30 17:35:44 +0200

    Add helper class for handling background tasks in GUI

---
+++ native/sentools/gui/gui_background_task.h
@@ -0,0 +1,87 @@
+#pragma once
+
+#include <atomic>
+#include <cassert>
+#include <functional>
+#include <future>
+#include <optional>
+#include <thread>
+#include <utility>
+
+#include "util/scope.h"
+
+namespace SenTools::GUI {
+template<typename ResultT, typename... ArgsT>
+struct BackgroundTask {
+    BackgroundTask(std::function<ResultT(ArgsT...)> workFunction)
+      : WorkFunction(std::move(workFunction)) {}
+    BackgroundTask(const BackgroundTask& other) = delete;
+    BackgroundTask(BackgroundTask&& other) = delete;
+    BackgroundTask& operator=(const BackgroundTask& other) = delete;
+    BackgroundTask& operator=(BackgroundTask&& other) = delete;
+    ~BackgroundTask() {
+        if (Engaged()) {
+            if (Data->Thread.joinable()) {
+                Data->Thread.join();
+            }
+            Data.reset();
+        }
+    }
+
+    // Returns true if Task is in progress or has result available.
+    bool Engaged() const {
+        return Data.has_value();
+    }
+
+    // Returns true if result is available.
+    bool ResultAvailable() const {
+        return Engaged() && Data->Done.load(std::memory_order_acquire);
+    }
+
+    // Start the task
+    void Engage(ArgsT... args) {
+        assert(!Engaged());
+        Data.emplace(*this, std::move(args)...);
+    }
+
+    // Fetches the result and returns to the un-engaged state.
+    // Will block until the result is ready if the result is not ready yet!
+    // If the task has ended with a thrown exception it will be rethrown.
+    ResultT FetchResultAndDisengage() {
+        assert(Engaged());
+        auto resetGuard = HyoutaUtils::MakeScopeGuard([this] {
+            if (Data->Thread.joinable()) {
+                Data->Thread.join();
+            }
+            Data.reset();
+        });
+        return Data->Future.get();
+    }
+
+private:
+    struct ThreadData {
+        std::atomic<bool> Done;
+        std::promise<ResultT> Promise;
+        std::future<ResultT> Future;
+        std::thread Thread;
+
+        ThreadData(BackgroundTask& bgt, ArgsT... args)
+          : Done(false)
+          , Promise()
+          , Future(Promise.get_future())
+          , Thread(std::thread(std::bind(&BackgroundTask::ThreadFunc, &bgt, std::move(args)...))) {}
+    };
+    std::function<ResultT(ArgsT...)> WorkFunction;
+    std::optional<ThreadData> Data;
+
+    void ThreadFunc(ArgsT... args) {
+        auto doneGuard = HyoutaUtils::MakeScopeGuard(
+            [this] { Data->Done.store(true, std::memory_order_release); });
+        try {
+            Data->Promise.set_value(WorkFunction(std::move(args)...));
+        } catch (...) {
+            Data->Promise.set_exception(std::current_exception());
+        }
+    }
+};
+} // namespace SenTools::GUI
